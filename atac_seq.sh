803a72affa7b24f6e8192777cd0be033df7b4829
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-02-21 21:14:34 +0800

    Create atac_seq.sh

---
+++ atac_seq.sh
@@ -0,0 +1,72 @@
+
+/root/myPrograms/STAR/bin/STAR --genomeDir /root/resources/star_hg38_gencode27_overhang50 \
+--readFilesCommand zcat \
+--runThreadN 35 \
+--alignIntronMax 1 \
+--alignEndsType EndToEnd \
+--readFilesIn /root/ong_dukenus/Dereck/1_3502DukeNus_TS543-NT-031117_hs_i9_r1.fastq.gz \
+/root/ong_dukenus/Dereck/1_3502DukeNus_TS543-NT-031117_hs_i9_r2.fastq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/ong_dukenus/bam/1_3502DukeNus_TS543-NT-031117_hs_i9_
+
+/root/myPrograms/STAR/bin/STAR --genomeDir /root/resources/star_hg38_gencode27_overhang50 \
+--readFilesCommand zcat \
+--runThreadN 35 \
+--alignIntronMax 1 \
+--alignEndsType EndToEnd \
+--readFilesIn /root/ong_dukenus/Dereck/2_3502DukeNus_TS543-143-031117_hs_i10_r1.fastq.gz \
+/root/ong_dukenus/Dereck/2_3502DukeNus_TS543-143-031117_hs_i10_r2.fastq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/ong_dukenus/bam/2_3502DukeNus_TS543-143-031117_hs_i10_
+
+/root/myPrograms/STAR/bin/STAR --genomeDir /root/resources/star_hg38_gencode27_overhang50 \
+--readFilesCommand zcat \
+--runThreadN 35 \
+--alignIntronMax 1 \
+--alignEndsType EndToEnd \
+--readFilesIn /root/ong_dukenus/Dereck/3_3502DukeNus_TS543-400-031117_hs_i11_r1.fastq.gz \
+/root/ong_dukenus/Dereck/3_3502DukeNus_TS543-400-031117_hs_i11_r2.fastq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/ong_dukenus/bam/3_3502DukeNus_TS543-400-031117_hs_i11_
+
+/root/myPrograms/STAR/bin/STAR --genomeDir /root/resources/star_hg38_gencode27_overhang50 \
+--readFilesCommand zcat \
+--runThreadN 35 \
+--alignIntronMax 1 \
+--alignEndsType EndToEnd \
+--readFilesIn /root/ong_dukenus/Dereck/4_3502DukeNus_TS543-NT-241117_hs_i12_r1.fastq.gz \
+/root/ong_dukenus/Dereck/4_3502DukeNus_TS543-NT-241117_hs_i12_r1.fastq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/ong_dukenus/bam/4_3502DukeNus_TS543-NT-241117_hs_i12_
+
+/root/myPrograms/STAR/bin/STAR --genomeDir /root/resources/star_hg38_gencode27_overhang50 \
+--readFilesCommand zcat \
+--runThreadN 35 \
+--alignIntronMax 1 \
+--alignEndsType EndToEnd \
+--readFilesIn /root/ong_dukenus/Dereck/4_3502DukeNus_TS543-NT-241117_hs_i12_r1.fastq.gz \
+/root/ong_dukenus/Dereck/4_3502DukeNus_TS543-NT-241117_hs_i12_r2.fastq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/ong_dukenus/bam/4_3502DukeNus_TS543-NT-241117_hs_i12_
+
+/root/myPrograms/STAR/bin/STAR --genomeDir /root/resources/star_hg38_gencode27_overhang50 \
+--readFilesCommand zcat \
+--runThreadN 35 \
+--alignIntronMax 1 \
+--alignEndsType EndToEnd \
+--readFilesIn /root/ong_dukenus/Dereck/5_3502DukeNus_TS543-143-241117_hs_i13_r1.fastq.gz \
+/root/ong_dukenus/Dereck/5_3502DukeNus_TS543-143-241117_hs_i13_r2.fastq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/ong_dukenus/bam/5_3502DukeNus_TS543-143-241117_hs_i13_
+
+/root/myPrograms/STAR/bin/STAR --genomeDir /root/resources/star_hg38_gencode27_overhang50 \
+--readFilesCommand zcat \
+--runThreadN 35 \
+--alignIntronMax 1 \
+--alignEndsType EndToEnd \
+--readFilesIn /root/ong_dukenus/Dereck/6_3502DukeNus_TS543-400-241117_hs_i14_r1.fastq.gz \
+/root/ong_dukenus/Dereck/6_3502DukeNus_TS543-400-241117_hs_i14_r2.fastq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/ong_dukenus/bam/6_3502DukeNus_TS543-400-241117_hs_i14_
+
+
