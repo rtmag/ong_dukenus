416f2def7f81734e7ce5467f5cf9a7b9f7e896d5
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-02-27 19:40:18 +0800

    Create paul_aligment_post_processing.bash

---
+++ paul_aligment_post_processing.bash
@@ -0,0 +1,34 @@
+java -jar /root/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
+I=/root/ong_dukenus/paul_bam/1_3502DukeNus_TS543-NT-031117_hg19_i9.bam \
+O=/root/ong_dukenus/paul_bam/1_3502DukeNus_TS543-NT-031117_hg19_i9_rmdup.bam \
+M=/root/ong_dukenus/paul_bam/1_3502DukeNus_TS543-NT-031117_hg19_i9.mfile
+
+java -jar /root/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
+I=/root/ong_dukenus/paul_bam/2_3502DukeNus_TS543-143-031117_hg19_i10.bam \
+O=/root/ong_dukenus/paul_bam/2_3502DukeNus_TS543-143-031117_hg19_i10_rmdup.bam \
+M=/root/ong_dukenus/paul_bam/2_3502DukeNus_TS543-143-031117_hg19_i10.mfile
+
+java -jar /root/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
+I=/root/ong_dukenus/paul_bam/3_3502DukeNus_TS543-400-031117_hg19_i11.bam \
+O=/root/ong_dukenus/paul_bam/3_3502DukeNus_TS543-400-031117_hg19_i11_rmdup.bam \
+M=/root/ong_dukenus/paul_bam/3_3502DukeNus_TS543-400-031117_hg19_i11.mfile
+
+java -jar /root/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
+I=/root/ong_dukenus/paul_bam/4_3502DukeNus_TS543-NT-241117_hg19_i12.bam \
+O=/root/ong_dukenus/paul_bam/4_3502DukeNus_TS543-NT-241117_hg19_i12_rmdup.bam \
+M=/root/ong_dukenus/paul_bam/4_3502DukeNus_TS543-NT-241117_hg19_i12.mfile
+
+java -jar /root/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
+I=/root/ong_dukenus/paul_bam/5_3502DukeNus_TS543-143-241117_hg19_i13.bam \
+O=/root/ong_dukenus/paul_bam/5_3502DukeNus_TS543-143-241117_hg19_i13_rmdup.bam \
+M=/root/ong_dukenus/paul_bam/5_3502DukeNus_TS543-143-241117_hg19_i13.mfile
+
+java -jar /root/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
+I=/root/ong_dukenus/paul_bam/6_3502DukeNus_TS543-400-241117_hg19_i14.bam \
+O=/root/ong_dukenus/paul_bam/6_3502DukeNus_TS543-400-241117_hg19_i14_rmdup.bam \
+M=/root/ong_dukenus/paul_bam/6_3502DukeNus_TS543-400-241117_hg19_i14.mfile
+
+
+
+
+
