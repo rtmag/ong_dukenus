625a3fc31ba045d0c23db00e189244290c6ea509
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-02-28 01:48:48 +0800

    Update paul_diff_bind.R

--- paul_diff_bind.R
+++ paul_diff_bind.R
@@ -1,8 +1,7 @@
 
 library(Rsubread)
-
+#
 x=read.table('/root/ong_dukenus/paul_peakcalls/atac_merged_broadPeak.bed',sep="\t",stringsAsFactors=F)
-
 ann = data.frame(GeneID=paste(x[,1],x[,2],x[,3],sep="_!_"),Chr=x[,1],Start=x[,2],End=x[,3],Strand='+')
 
 bam.files <- c("/root/ong_dukenus/paul_bam/1_3502DukeNus_TS543-NT-031117_hg19_i9_rmdup.bam",
@@ -13,25 +12,25 @@ bam.files <- c("/root/ong_dukenus/paul_bam/1_3502DukeNus_TS543-NT-031117_hg19_i9
 "/root/ong_dukenus/paul_bam/6_3502DukeNus_TS543-400-241117_hg19_i14_rmdup.bam")
 
 fc_SE <- featureCounts(bam.files,annot.ext=ann,isPairedEnd=TRUE,nthreads=20)
-
 countData=fc_SE$counts
-
 colnames(countData)=c("1_NT","2_143","3_400","4_NT","5_143","6_400")
-
 saveRDS(countData,'atac_broadPeak_countdata.rds')
 ########################################################################################################
 ########################################################################################################
 x=read.table('/root/ong_dukenus/paul_peakcalls/atac_merged_narrowPeak.bed',sep="\t",stringsAsFactors=F)
-
 ann = data.frame(GeneID=paste(x[,1],x[,2],x[,3],sep="_!_"),Chr=x[,1],Start=x[,2],End=x[,3],Strand='+')
-
 fc_SE <- featureCounts(bam.files,annot.ext=ann,isPairedEnd=TRUE,nthreads=20)
-
 countData=fc_SE$counts
-
 colnames(countData)=c("1_NT","2_143","3_400","4_NT","5_143","6_400")
-
 saveRDS(countData,'atac_narrowPeak_countdata.rds')
 
 ########################################################################################################
 ########################################################################################################
+
+x=read.table('/root/ong_dukenus/paul_peakcalls/atac_merged_broadPeak.bed',sep="\t",stringsAsFactors=F)
+ann = data.frame(GeneID=paste(x[,1],x[,2],x[,3],sep="_!_"),Chr=x[,1],Start=x[,2],End=x[,3],Strand='+')
+
+bam.test <- c("/root/ong_dukenus/paul_bam/1_3502DukeNus_TS543-NT-031117_hg19_i9_rmdup.bam")
+
+fc_SE <- featureCounts(bam.files,annot.ext=ann,isPairedEnd=TRUE,nthreads=20,allowMultiOverlap=TRUE)
+
