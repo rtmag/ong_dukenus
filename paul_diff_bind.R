0ced07ca11cf5f560cb74926a0533e3b0359061f
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-02-28 02:48:57 +0800

    Update paul_diff_bind.R

--- paul_diff_bind.R
+++ paul_diff_bind.R
@@ -27,10 +27,39 @@ saveRDS(countData,'atac_narrowPeak_countdata.rds')
 ########################################################################################################
 ########################################################################################################
 
-x=read.table('/root/ong_dukenus/paul_peakcalls/atac_merged_broadPeak.bed',sep="\t",stringsAsFactors=F)
-ann = data.frame(GeneID=paste(x[,1],x[,2],x[,3],sep="_!_"),Chr=x[,1],Start=x[,2],End=x[,3],Strand='+')
+countData=readRDS('atac_broadPeak_countdata.rds')
 
-bam.test <- c("/root/ong_dukenus/paul_bam/1_3502DukeNus_TS543-NT-031117_hg19_i9_rmdup.bam")
+require(DESeq2)
 
-fc_SE <- featureCounts(bam.files,annot.ext=ann,isPairedEnd=TRUE,nthreads=20,allowMultiOverlap=TRUE)
+colData <- data.frame(group=c("NT","143","400","NT","143","400") )
+dds <- DESeqDataSetFromMatrix(
+       countData = countData,
+       colData = colData,
+       design = ~ group)
+
+dLRT <- DESeq(dds, test="LRT", reduced=~1)
+dLRT_vsd <- varianceStabilizingTransformation(dLRT)
+
+pdf("Diagnostic_design_pca_broadPeak.pdf")
+plotPCA(dLRT_vsd,ntop=136800,intgroup=c('group'))
+dev.off()
+
+countData=readRDS('atac_narrowPeak_countdata.rds')
+
+require(DESeq2)
+
+colData <- data.frame(group=c("NT","143","400","NT","143","400") )
+dds <- DESeqDataSetFromMatrix(
+       countData = countData,
+       colData = colData,
+       design = ~ group)
+
+dLRT <- DESeq(dds, test="LRT", reduced=~1)
+dLRT_vsd <- varianceStabilizingTransformation(dLRT)
+
+pdf("Diagnostic_design_pca_broadPeak.pdf")
+plotPCA(dLRT_vsd,ntop=136800,intgroup=c('group'))
+dev.off()
+
+
 
