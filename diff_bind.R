b08b42453f306de60bcead0c852a03a8cb853c43
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-03-01 10:39:10 +0800

    Update diff_bind.R

--- diff_bind.R
+++ diff_bind.R
@@ -1,89 +1,28 @@
-
-# cat 1_3502DukeNus_TS543-NT-031117_hs_i9_peaks.broadPeak \
-# 3_3502DukeNus_TS543-400-031117_hs_i11_peaks.broadPeak \
-# 4_3502DukeNus_TS543-NT-241117_hs_i12_peaks.broadPeak \
-# 5_3502DukeNus_TS543-143-241117_hs_i13_peaks.broadPeak \
-# 6_3502DukeNus_TS543-400-241117_hs_i14_peaks.broadPeak | \
-# sort -k1,1 -k2,2n |bedtools merge -i - > dukenus_merged_peaks.bed
-
-
-
-bed_to_granges <- function(file){
-   df <- read.table(file,
-                    header=F,
-                    stringsAsFactors=F)
- 
-   if(length(df) > 6){
-      df <- df[,-c(7:length(df))]
-   }
- 
-   if(length(df)<3){
-      stop("File has less than 3 columns")
-   }
- 
-   header <- c('chr','start','end','id','score','strand')
-   names(df) <- header[1:length(names(df))]
- 
-   if('strand' %in% colnames(df)){
-      df$strand <- gsub(pattern="[^+-]+", replacement = '*', x = df$strand)
-   }
- 
-   library("GenomicRanges")
- 
-   if(length(df)==3){
-      gr <- with(df, GRanges(chr, IRanges(start, end)))
-   } else if (length(df)==4){
-      gr <- with(df, GRanges(chr, IRanges(start, end), id=id))
-   } else if (length(df)==5){
-      gr <- with(df, GRanges(chr, IRanges(start, end), id=id, score=score))
-   } else if (length(df)==6){
-      gr <- with(df, GRanges(chr, IRanges(start, end), id=id, score=score, strand=strand))
-   }
-   return(gr)
-}
-
+#library(Rsubread)
 #
-require(csaw)
-
-param <- readParam(minq=10, pe='both')
-
-regions=bed_to_granges("/root/ong_dukenus/peakcalls/dukenus_merged_peaks_noBlackList.bed")
-
-
-counts <- regionCounts(bam.files, regions, param=param)
-##
-#
-library(Rsubread)
-
-x=read.table('/root/ong_dukenus/peakcalls/dukenus_merged_peaks_noBlackList.bed',sep="\t",stringsAsFactors=F)
-
+x=read.table('/root/ong_dukenus/peakcalls/star_merged_broad_noBlackList.bed',sep="\t",stringsAsFactors=F)
 ann = data.frame(GeneID=paste(x[,1],x[,2],x[,3],sep="_!_"),Chr=x[,1],Start=x[,2],End=x[,3],Strand='+')
 
-bam.files <- c("/root/ong_dukenus/bam/1_3502DukeNus_TS543-NT-031117_hs_i9_Aligned.sortedByCoord.rmdup.out.bam",
-"/root/ong_dukenus/bam/3_3502DukeNus_TS543-400-031117_hs_i11_Aligned.sortedByCoord.rmdup.out.bam",
-"/root/ong_dukenus/bam/4_3502DukeNus_TS543-NT-241117_hs_i12_Aligned.sortedByCoord.rmdup.out.bam",
-"/root/ong_dukenus/bam/5_3502DukeNus_TS543-143-241117_hs_i13_Aligned.sortedByCoord.rmdup.out.bam",
-"/root/ong_dukenus/bam/6_3502DukeNus_TS543-400-241117_hs_i14_Aligned.sortedByCoord.rmdup.out.bam")
-
-
-
+bam.files <- c("/root/ong_dukenus/bam/1_NT_Aligned.sortedByCoord.rmdup.out.bam",
+"/root/ong_dukenus/bam/2_143_Aligned.sortedByCoord.rmdup.out.bam",
+"/root/ong_dukenus/bam/3_400_Aligned.sortedByCoord.rmdup.out.bam",
+"/root/ong_dukenus/bam/4_NT_Aligned.sortedByCoord.rmdup.out.bam",
+"/root/ong_dukenus/bam/5_143_Aligned.sortedByCoord.rmdup.out.bam",
+"/root/ong_dukenus/bam/6_400_Aligned.sortedByCoord.rmdup.out.bam")
 
 fc_SE <- featureCounts(bam.files,annot.ext=ann,isPairedEnd=TRUE,nthreads=20)
-
-#
-##
-
 countData=fc_SE$counts
+colnames(countData)=c("1_NT","2_143","3_400","4_NT","5_143","6_400")
+saveRDS(countData,'atac_broadPeak_countdata.rds')
 
-colnames(countData)=c("1_NT","3_400","4_NT","5_143","6_400")
+########################################################################################################
+########################################################################################################
 
-saveRDS(countData,'atac_countdata.rds')
-
-countData=readRDS('atac_countdata.rds')
+countData=readRDS('atac_broadPeak_countdata.rds')
 
 require(DESeq2)
 
-colData <- data.frame(group=colnames(countData) )
+colData <- data.frame(group=c("NT","143","400","NT","143","400") )
 dds <- DESeqDataSetFromMatrix(
        countData = countData,
        colData = colData,
@@ -92,8 +31,63 @@ dds <- DESeqDataSetFromMatrix(
 dLRT <- DESeq(dds, test="LRT", reduced=~1)
 dLRT_vsd <- varianceStabilizingTransformation(dLRT)
 
-
-pdf("Diagnostic_design_pca.pdf")
-plotPCA(dLRT_vsd,ntop=136500,intgroup=c('group'))
+pdf("Diagnostic_design_pca_broadPeak.pdf")
+plotPCA(dLRT_vsd,ntop=136800,intgroup=c('group'))
 dev.off()
 
+########################################################################################################
+########################################################################################################
+
+require(DESeq2)
+library(ggplot2)
+
+design<-data.frame(cells = c("NT","143","NT","143") )
+
+dds <- DESeqDataSetFromMatrix(countData = countData[,c(1,2,4,5)], colData = design, design = ~ cells)
+dds <- DESeq(dds)
+res <- results(dds, contrast=c("cells","NT","143"))
+bed_NT = t(matrix(unlist(strsplit(rownames(res[which(res$log2FoldChange>1 & res$padj<0.05),]),"_!_")),nrow=3))
+bed_143 = t(matrix(unlist(strsplit(rownames(res[which(res$log2FoldChange<(-1) & res$padj<0.05),]),"_!_")),nrow=3))
+
+write.table(bed_NT,"NT_over_143.bed",sep="\t",quote=F,row.names=F,col.names=F)
+write.table(bed_143,"143_over_NT.bed",sep="\t",quote=F,row.names=F,col.names=F)
+
+
+pdf("Volcano_NT_vs_143.pdf")
+plot(res$log2FoldChange,-log10(res$padj),xlab=expression('Log'[2]*' Fold Change ( NT / 143 ) '),
+              ylab=expression('-Log'[10]*' Q-values'),col=alpha("grey",.04))
+abline(v=-1,lty = 2,col="grey")
+abline(v=1,lty = 2,col="grey")
+abline(h=-log10(0.05),lty = 2,col="grey")
+points(res$log2FoldChange[abs(res$log2FoldChange)>1 & res$padj<0.05],
+       -log10(res$padj)[abs(res$log2FoldChange)>1 & res$padj<0.05],
+      col=alpha("#c0392b",.05))
+legend("topright", paste("NT :",length(which(res$log2FoldChange>1 & res$padj<0.05))), bty="n") 
+legend("topleft", paste("143 :",length(which(res$log2FoldChange<(-1) & res$padj<0.05))), bty="n") 
+dev.off()
+#
+design<-data.frame(cells = c("NT","400","NT","400") )
+
+dds <- DESeqDataSetFromMatrix(countData = countData[,c(1,3,4,6)], colData = design, design = ~ cells)
+dds <- DESeq(dds)
+res <- results(dds, contrast=c("cells","NT","400"))
+bed_NT = t(matrix(unlist(strsplit(rownames(res[which(res$log2FoldChange>1 & res$padj<0.05),]),"_!_")),nrow=3))
+bed_400 = t(matrix(unlist(strsplit(rownames(res[which(res$log2FoldChange<(-1) & res$padj<0.05),]),"_!_")),nrow=3))
+
+write.table(bed_NT,"NT_over_400.bed",sep="\t",quote=F,row.names=F,col.names=F)
+write.table(bed_400,"400_over_NT.bed",sep="\t",quote=F,row.names=F,col.names=F)
+
+
+pdf("Volcano_NT_vs_400.pdf")
+plot(res$log2FoldChange,-log10(res$padj),xlab=expression('Log'[2]*' Fold Change ( NT / 400 ) '),
+              ylab=expression('-Log'[10]*' Q-values'),col=alpha("grey",.04))
+abline(v=-1,lty = 2,col="grey")
+abline(v=1,lty = 2,col="grey")
+abline(h=-log10(0.05),lty = 2,col="grey")
+points(res$log2FoldChange[abs(res$log2FoldChange)>1 & res$padj<0.05],
+       -log10(res$padj)[abs(res$log2FoldChange)>1 & res$padj<0.05],
+      col=alpha("#c0392b",.05))
+legend("topright", paste("NT :",length(which(res$log2FoldChange>1 & res$padj<0.05))), bty="n") 
+legend("topleft", paste("400 :",length(which(res$log2FoldChange<(-1) & res$padj<0.05))), bty="n") 
+dev.off()
+#
