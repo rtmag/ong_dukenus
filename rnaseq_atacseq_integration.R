b91666e98e910c4fc50a2868314b2a151339ec7c
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-03-11 03:49:34 +0800

    Update rnaseq_atacseq_integration.R

--- rnaseq_atacseq_integration.R
+++ rnaseq_atacseq_integration.R
@@ -50,6 +50,44 @@ hg19=read.table('~/resources/hg19_tss_knownCanonical_noUnasembled.bed',sep="\t",
 gene_symbol=read.table('gene_symbol.txt',sep="\t",stringsAsFactors=F)
 gene_tss = hg19[(which(hg19[,4] %in% gene_symbol[,1])),]
 gene_tss = gene_tss[!duplicated(gene_tss[,4]),]
+gene_tss_1kb=data.frame(gene_tss[,1],gene_tss[,2]-500,gene_tss[,3]+500,gene_tss[,4:6])
+
+write.table(gene_tss_1kb,"gene_tss_1kb.bed",sep="\t",quote=F,col.names=F,row.names=F)
+
+bed_to_granges <- function(file){
+   df <- read.table(file,
+                    header=F,
+                    stringsAsFactors=F)
+ 
+   if(length(df) > 6){
+      df <- df[,-c(7:length(df))]
+   }
+ 
+   if(length(df)<3){
+      stop("File has less than 3 columns")
+   }
+ 
+   header <- c('chr','start','end','id','score','strand')
+   names(df) <- header[1:length(names(df))]
+ 
+   if('strand' %in% colnames(df)){
+      df$strand <- gsub(pattern="[^+-]+", replacement = '*', x = df$strand)
+   }
+ 
+   library("GenomicRanges")
+ 
+   if(length(df)==3){
+      gr <- with(df, GRanges(chr, IRanges(start, end)))
+   } else if (length(df)==4){
+      gr <- with(df, GRanges(chr, IRanges(start, end), id=id))
+   } else if (length(df)==5){
+      gr <- with(df, GRanges(chr, IRanges(start, end), id=id, score=score))
+   } else if (length(df)==6){
+      gr <- with(df, GRanges(chr, IRanges(start, end), id=id, score=score, strand=strand))
+   }
+   return(gr)
+}
+#
 
 
 require(csaw)
@@ -57,11 +95,11 @@ require(csaw)
 blacklist=bed_to_granges("~/resources/hg19consensusBlacklist.bed")
 param <- readParam(minq=10,discard=blacklist)
 bam.files <- c(
-"/root/ong_dukenus/paul_bam/1_3502DukeNus_TS543-NT-031117_hg19_i9_rmdup.bam"
-"/root/ong_dukenus/paul_bam/2_3502DukeNus_TS543-143-031117_hg19_i10_rmdup.bam"
-"/root/ong_dukenus/paul_bam/3_3502DukeNus_TS543-400-031117_hg19_i11_rmdup.bam"
-"/root/ong_dukenus/paul_bam/4_3502DukeNus_TS543-NT-241117_hg19_i12_rmdup.bam"
-"/root/ong_dukenus/paul_bam/5_3502DukeNus_TS543-143-241117_hg19_i13_rmdup.bam"
+"/root/ong_dukenus/paul_bam/1_3502DukeNus_TS543-NT-031117_hg19_i9_rmdup.bam",
+"/root/ong_dukenus/paul_bam/2_3502DukeNus_TS543-143-031117_hg19_i10_rmdup.bam",
+"/root/ong_dukenus/paul_bam/3_3502DukeNus_TS543-400-031117_hg19_i11_rmdup.bam",
+"/root/ong_dukenus/paul_bam/4_3502DukeNus_TS543-NT-241117_hg19_i12_rmdup.bam",
+"/root/ong_dukenus/paul_bam/5_3502DukeNus_TS543-143-241117_hg19_i13_rmdup.bam",
 "/root/ong_dukenus/paul_bam/6_3502DukeNus_TS543-400-241117_hg19_i14_rmdup.bam"
 )
 
@@ -69,10 +107,10 @@ binned <- windowCounts(bam.files, bin=TRUE, width=10000, param=param)
 
 normfacs <- normOffsets(binned)
 ##
-
+regions=bed_to_granges("gene_tss_1kb.bed")
 counts <- regionCounts(bam.files, regions, ext=300, param=param)
 countData=assay(counts)
-colnames(countData)=bam.files
+colnames(countData)=c("shNT_1","shH2AFV#1_1","shH2AFV#2_1","shNT_2","shH2AFV#1_2","shH2AFV#2_2")
 
 require(DESeq2)
 
