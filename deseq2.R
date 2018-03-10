a35f17d8c35cb3fe798d7d353485ce46fbfa8505
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-03-10 22:48:40 +0800

    Update deseq2.R

--- deseq2.R
+++ deseq2.R
@@ -4,7 +4,7 @@ colnames(countData) = c("202_1","202_2","202_3","143_1","143_2","143_3","400_1",
 options(scipen=999)
 library(DESeq2)
 
-
+#############################################################################
 colData <- data.frame(condition = c("shNT","shNT","shNT","shH2AFV#1","shH2AFV#1","shH2AFV#1","shH2AFV#2","shH2AFV#2","shH2AFV#2") )
 dds <- DESeqDataSetFromMatrix(
        countData = countData,
@@ -17,7 +17,7 @@ dLRT_vsd <- varianceStabilizingTransformation(dLRT)
 pdf("Diagnostic_design_pca.pdf")
 plotPCA(dLRT_vsd,ntop=60000,intgroup=c('condition'))
 dev.off()
-
+#############################################################################
 # shH2AFV VS shNT
 design<-data.frame(experiment=colnames(countData), 
                    sh = c("shNT","shNT","shNT","shH2AFV","shH2AFV","shH2AFV","shH2AFV","shH2AFV","shH2AFV") )
@@ -30,7 +30,7 @@ dds_res <- results(dds,contrast=c("sh","shH2AFV","shNT"))
 sig_vsd=dds_vsd[which(dds_res$padj<0.05 & abs(dds_res$log2FoldChange)>1),]
 sig_vsd = assay(sig_vsd)
 colnames(sig_vsd) = c("shNT","shNT","shNT","shH2AFV#1","shH2AFV#1","shH2AFV#1","shH2AFV#2","shH2AFV#2","shH2AFV#2")
-
+#############################################################################
 library(gplots)
 library(factoextra)
       
@@ -69,13 +69,15 @@ names(dw_reg_log) = rownames(dw_reg)
 
 rankedlist = cbind(sort(c(up_reg_log,dw_reg_log),decreasing=T) )
 rankedlist = data.frame(ensid=rownames(rankedlist), log10FDR=rankedlist)
+#############################################################################
+# Annotation
 
 library(ensembldb)
 library("EnsDb.Hsapiens.v86")   
 edb <- EnsDb.Hsapiens.v86
 tx <- genes(edb, columns=c("gene_id", "gene_name"))
 tx=data.frame(gene_id=tx$gene_id, gene_name=tx$gene_name)
-
+#############################################################################
 rankedlist[["ensid"]] <- tx[ match(rankedlist[['ensid']], tx[['gene_id']] ) , 'gene_name']
 
 write.table(rankedlist,"genes_ranked_table_FCFDR.rnk", sep="\t", quote=F,col.names=F,row.names=F)
@@ -91,17 +93,19 @@ dds_res = data.frame(dds_res,gene_symbol=rownames(dds_res))
 dds_res[["gene_symbol"]] <- tx[ match(dds_res[['gene_symbol']], tx[['gene_id']] ) , 'gene_name']
 
 write.csv(dds_res[order(dds_res$log2FoldChange),],"deseq2_results.csv")
-
+#############################################################################
 ## qpcr
-pdf("QC_genes.pdf")
+pdf("QC_genes.pdf", width=15, height=6)
+
 vsd = assay(dds_vsd)
 rownames(vsd) = tx[ match(rownames(vsd), tx[['gene_id']] ) , 'gene_name']
 
-notch=dds_res[grep("NOTCH",dds_res$gene_symbol),2]
-names(notch) = as.character(dds_res[grep("NOTCH",dds_res$gene_symbol),7])
+notch=dds_res[grep("NOTCH",rownames(vsd)),2]
+names(notch) = as.character(rownames(vsd[grep("NOTCH",rownames(vsd)),]))
 notch = notch[!is.na(notch)]
 
-barplot(notch,ylim=c(-1,.5),col="#bae1ff")
+par(mar=c(5.1,4.1,4.1,2.1))
+barplot(notch,ylim=c(-1.5,1.5),col="#bae1ff",ylab="Log2 Fold Change")
 abline(h=0)
 
 barplot(t(vsd[rownames(vsd) %in% names(notch),]),beside=T,ylim=c(0,15),ylab="Log2 Normalized Counts",col=c("#ffb3ba","#ffb3ba","#ffb3ba",
@@ -110,13 +114,16 @@ barplot(t(vsd[rownames(vsd) %in% names(notch),]),beside=T,ylim=c(0,15),ylab="Log
 legend("topright",c("202","143","400"),fill=c("#ffb3ba","#baffc9","#bae1ff") )
 abline(h=0)
 
+###
 
-h2a=dds_res[grep("h2a",dds_res$gene_symbol,ignore.case=T),2]
-names(h2a) = as.character(dds_res[grep("h2a",dds_res$gene_symbol,ignore.case=T),7])
+h2a=dds_res[grep("h2a",rownames(vsd),ignore.case=T),2]
+names(h2a) = as.character( rownames( vsd[grep("h2a",rownames(vsd),ignore.case=T),]) )
 h2a = h2a[!is.na(h2a)]
 h2a = h2a[abs(h2a)>.2]
 
-barplot(h2a,ylim=c(-1.5,1.5),col="#966FD6")
+# bottom, left, top and right margins respectively (5.1,4.1,4.1,2.1
+par(mar=c(5.1,4.1,4.1,2.1))
+barplot(h2a,ylim=c(-1.5,1.5),col="#966FD6",ylab="Log2 Fold Change")
 abline(h=0)
 
 barplot(t(vsd[rownames(vsd) %in% names(h2a),]),beside=T,ylim=c(0,15),ylab="Log2 Normalized Counts",col=c("#ffb3ba","#ffb3ba","#ffb3ba",
