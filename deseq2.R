4d0a6d27f2043c95471ef637f14dc834be41d6c3
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-03-13 12:07:12 +0800

    Update deseq2.R

--- deseq2.R
+++ deseq2.R
@@ -27,7 +27,7 @@ dds <- DESeq(dds)
 dds_vsd <- varianceStabilizingTransformation(dds)
 dds_res <- results(dds,contrast=c("sh","shH2AFV","shNT"))
 
-sig_vsd=dds_vsd[which(dds_res$padj<0.05 & abs(dds_res$log2FoldChange)>1),]
+sig_vsd=dds_vsd[which(dds_res$padj<0.05 & abs(dds_res$log2FoldChange)>.6),]
 sig_vsd = assay(sig_vsd)
 colnames(sig_vsd) = c("shNT","shNT","shNT","shH2AFV#1","shH2AFV#1","shH2AFV#1","shH2AFV#2","shH2AFV#2","shH2AFV#2")
 #############################################################################
@@ -37,25 +37,25 @@ library(factoextra)
  library(RColorBrewer)
 colors <- colorRampPalette(c("blue","white","red"))(45)
 
-pdf("heatmap_differentially_expressed_genes.pdf")
+pdf("heatmap_differentially_expressed_genes_log2fc.5.pdf")
 heatmap.2(sig_vsd,col=colors,scale="row", trace="none",distfun = function(x) get_dist(x,method="spearman"),srtCol=25,
           labRow = FALSE,xlab="", ylab="Genes",key.title="Gene expression")
 dev.off()
 
-pdf("volcano_plot_differentially_expressed_genes.pdf")
+pdf("volcano_plot_differentially_expressed_genes_log2fc.5.pdf")
 plot(dds_res$log2FoldChange,-log10(dds_res$padj),xlab=expression('Log'[2]*' Fold Change ( shH2AFV / shNT )'),
               ylab=expression('-Log'[10]*' Q-values'),col=alpha("grey",.5))
-abline(v=-1,lty = 2,col="grey")
-abline(v=1,lty = 2,col="grey")
+abline(v=-.5,lty = 2,col="grey")
+abline(v=.5,lty = 2,col="grey")
 abline(h=-log10(0.05),lty = 2,col="grey")
-points(dds_res$log2FoldChange[abs(dds_res$log2FoldChange)>1 & dds_res$padj<0.05],
-       -log10(dds_res$padj)[abs(dds_res$log2FoldChange)>1 & dds_res$padj<0.05],
+points(dds_res$log2FoldChange[abs(dds_res$log2FoldChange)>.5 & dds_res$padj<0.05],
+       -log10(dds_res$padj)[abs(dds_res$log2FoldChange)>.5 & dds_res$padj<0.05],
       col="red")
-legend("topright", paste("shH2AFV:",length(which(dds_res$log2FoldChange>1 & dds_res$padj<0.05))), bty="n") 
-legend("topleft", paste("shNT:",length(which(dds_res$log2FoldChange<(-1) & dds_res$padj<0.05))), bty="n") 
+legend("topright", paste("shH2AFV:",length(which(dds_res$log2FoldChange>.5 & dds_res$padj<0.05))), bty="n") 
+legend("topleft", paste("shNT:",length(which(dds_res$log2FoldChange<(-.5) & dds_res$padj<0.05))), bty="n") 
 dev.off()
 
-write.csv(dds_res[which(dds_res$padj<0.05 & abs(dds_res$log2FoldChange)>1),],"differentially_expressed_genes.csv")
+write.csv(dds_res[which(dds_res$padj<0.05 & abs(dds_res$log2FoldChange)>.5),],"differentially_expressed_genes_log2fc.5.csv")
 
 up_reg = dds_res[ which(dds_res$log2FoldChange>0),]
 up_reg = up_reg[ !is.na(up_reg$padj),]
