47c0d0a8befd910df4e44401241f1ba5ff97841a
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-03-14 02:10:00 +0800

    Update paul_volcanoPlot.R

--- paul_volcanoPlot.R
+++ paul_volcanoPlot.R
@@ -122,4 +122,93 @@ barplot(sort(enrich),las=2,ylim=c(-2.5,5),ylab="Log2 Enrichment",col="lightblue3
 abline(h=0)
 dev.off()
 
+###########################################################################################
+################################################################################################################
+pdf("pie_homer_Open_in_NT_noThreshold.pdf")
+res=read.table(pipe("more Down_NT.annStats |cut -f1,2,4"), sep="\t",header=F)
+i1 = which(res[,1]=="Annotation")[2]+1
+i2 = dim(res)[1]
+res = res[ i1:i2,]
+tdown = as.numeric(as.character(res[,2]))
+names(tdown) = res[,1]
+names(tdown) = paste(names(tdown)," ",round(tdown/sum(tdown)*100,digits=2),"%",sep="")
+pie(sort(tdown), main=,cex=.8)
+title("Distribution of chromatin regions open in NT compared to SH\n(23,466 regions)", cex.main=.9)
+dev.off()
 
+pdf("pie_homer_Open_in_SH_noThreshold.pdf")
+res=read.table(pipe("more Up_SH.annStats |cut -f1,2,4"), sep="\t",header=F)
+i1 = which(res[,1]=="Annotation")[2]+1
+i2 = dim(res)[1]
+res = res[ i1:i2,]
+tdown = as.numeric(as.character(res[,2]))
+names(tdown) = res[,1]
+names(tdown) = paste(names(tdown)," ",round(tdown/sum(tdown)*100,digits=2),"%",sep="")
+pie(sort(tdown), main=,cex=.8)
+title("Distribution of chromatin regions open in SH compared to NT\n(15,360 regions)", cex.main=.9)
+dev.off()
+
+####################################################################################
+#####
+pdf("enrichment_homer_in_NT_noThreshold.pdf")
+par(mar=c(11.1,4.1,4.1,2))
+res=read.table(pipe("more Down_NT.annStats |cut -f1,2,4"), sep="\t",header=F)
+i1 = which(res[,1]=="Annotation")[2]+1
+i2 = dim(res)[1]
+res = res[ i1:i2,]
+tdown = as.numeric(as.character(res[,2]))
+names(tdown) = res[,1]
+names(tdown) = paste(names(tdown)," ",round(tdown/sum(tdown)*100,digits=2),"%",sep="")
+
+enrich = as.numeric(as.character(res[,3]))
+tdown = tdown
+names(enrich) = names(tdown)
+barplot(sort(enrich),las=2,ylim=c(-2.5,5),ylab="Log2 Enrichment",col="lightblue3")
+abline(h=0)
+dev.off()
+
+pdf("enrichment_homer_in_SH_noThreshold.pdf")
+par(mar=c(11.1,4.1,4.1,2))
+res=read.table(pipe("more Up_SH.annStats |cut -f1,2,4"), sep="\t",header=F)
+i1 = which(res[,1]=="Annotation")[2]+1
+i2 = dim(res)[1]
+res = res[ i1:i2,]
+tdown = as.numeric(as.character(res[,2]))
+names(tdown) = res[,1]
+names(tdown) = paste(names(tdown)," ",round(tdown/sum(tdown)*100,digits=2),"%",sep="")
+
+enrich = as.numeric(as.character(res[,3]))
+tdown = tdown
+names(enrich) = names(tdown)
+barplot(sort(enrich),las=2,ylim=c(-2.5,5),ylab="Log2 Enrichment",col="lightblue3")
+abline(h=0)
+dev.off()
+
+
+###########################################################################################
+################################################################################################################
+pdf("pie_homer_Open_in_NT_white.pdf")
+res=read.table(pipe("more Down_NT.annStats |cut -f1,2,4"), sep="\t",header=F)
+i1 = which(res[,1]=="Annotation")[2]+1
+i2 = dim(res)[1]
+res = res[ i1:i2,]
+tdown = as.numeric(as.character(res[,2]))
+names(tdown) = res[,1]
+names(tdown) = paste(names(tdown)," ",round(tdown/sum(tdown)*100,digits=2),"%",sep="")
+tdown = tdown[tdown>200]
+pie(sort(tdown), main=,cex=.8,col=NA)
+title("Distribution of chromatin regions open in NT compared to SH\n(23,466 regions)", cex.main=.9)
+dev.off()
+
+pdf("pie_homer_Open_in_SH_white.pdf")
+res=read.table(pipe("more Up_SH.annStats |cut -f1,2,4"), sep="\t",header=F)
+i1 = which(res[,1]=="Annotation")[2]+1
+i2 = dim(res)[1]
+res = res[ i1:i2,]
+tdown = as.numeric(as.character(res[,2]))
+names(tdown) = res[,1]
+names(tdown) = paste(names(tdown)," ",round(tdown/sum(tdown)*100,digits=2),"%",sep="")
+tdown = tdown[tdown>200]
+pie(sort(tdown), main=,cex=.8,col=NA)
+title("Distribution of chromatin regions open in SH compared to NT\n(15,360 regions)", cex.main=.9)
+dev.off()
