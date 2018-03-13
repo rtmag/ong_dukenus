46202ab25023bcd813e9a975a757c51b8b0dc7fe
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-03-14 02:01:11 +0800

    Update paul_volcanoPlot.R

--- paul_volcanoPlot.R
+++ paul_volcanoPlot.R
@@ -57,3 +57,69 @@ names(tup) = paste(names(tup)," ",round(tup/sum(tup)*100,digits=2),"%",sep="")
 tup = tup[res[,2]>16]
 barplot(sort(tup),las=2,ylim=c(-2.5,5),ylab="Log2 Enrichment",col="lightblue3")
 dev.off()
+
+###########################################################################################
+################################################################################################################
+pdf("pie_homer_Open_in_NT.pdf")
+res=read.table(pipe("more Down_NT.annStats |cut -f1,2,4"), sep="\t",header=F)
+i1 = which(res[,1]=="Annotation")[2]+1
+i2 = dim(res)[1]
+res = res[ i1:i2,]
+tdown = as.numeric(as.character(res[,2]))
+names(tdown) = res[,1]
+names(tdown) = paste(names(tdown)," ",round(tdown/sum(tdown)*100,digits=2),"%",sep="")
+tdown = tdown[tdown>200]
+pie(sort(tdown), main=,cex=.8)
+title("Distribution of chromatin regions open in NT compared to SH\n(23,466 regions)", cex.main=.9)
+dev.off()
+
+pdf("pie_homer_Open_in_SH.pdf")
+res=read.table(pipe("more Up_SH.annStats |cut -f1,2,4"), sep="\t",header=F)
+i1 = which(res[,1]=="Annotation")[2]+1
+i2 = dim(res)[1]
+res = res[ i1:i2,]
+tdown = as.numeric(as.character(res[,2]))
+names(tdown) = res[,1]
+names(tdown) = paste(names(tdown)," ",round(tdown/sum(tdown)*100,digits=2),"%",sep="")
+tdown = tdown[tdown>200]
+pie(sort(tdown), main=,cex=.8)
+title("Distribution of chromatin regions open in SH compared to NT\n(15,360 regions)", cex.main=.9)
+dev.off()
+
+####################################################################################
+#####
+pdf("enrichment_homer_in_NT.pdf")
+par(mar=c(11.1,4.1,4.1,2))
+res=read.table(pipe("more Down_NT.annStats |cut -f1,2,4"), sep="\t",header=F)
+i1 = which(res[,1]=="Annotation")[2]+1
+i2 = dim(res)[1]
+res = res[ i1:i2,]
+tdown = as.numeric(as.character(res[,2]))
+names(tdown) = res[,1]
+names(tdown) = paste(names(tdown)," ",round(tdown/sum(tdown)*100,digits=2),"%",sep="")
+
+enrich = as.numeric(as.character(res[tdown>200,3]))
+tdown = tdown[tdown>200]
+names(enrich) = names(tdown)
+barplot(sort(enrich),las=2,ylim=c(-2.5,5),ylab="Log2 Enrichment",col="lightblue3")
+abline(h=0)
+dev.off()
+
+pdf("enrichment_homer_in_SH.pdf")
+par(mar=c(11.1,4.1,4.1,2))
+res=read.table(pipe("more Up_SH.annStats |cut -f1,2,4"), sep="\t",header=F)
+i1 = which(res[,1]=="Annotation")[2]+1
+i2 = dim(res)[1]
+res = res[ i1:i2,]
+tdown = as.numeric(as.character(res[,2]))
+names(tdown) = res[,1]
+names(tdown) = paste(names(tdown)," ",round(tdown/sum(tdown)*100,digits=2),"%",sep="")
+
+enrich = as.numeric(as.character(res[tdown>200,3]))
+tdown = tdown[tdown>200]
+names(enrich) = names(tdown)
+barplot(sort(enrich),las=2,ylim=c(-2.5,5),ylab="Log2 Enrichment",col="lightblue3")
+abline(h=0)
+dev.off()
+
+
