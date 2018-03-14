f40179962fc8b560bdcdb7c2c055c71cb4b71962
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-03-14 16:37:02 +0800

    Create bw_creation.sh

---
+++ bw_creation.sh
@@ -0,0 +1,45 @@
+samtools merge -f -h sh400-1.bam \
+sh400.bam \
+sh400-1.bam \
+sh400-2.bam \
+sh400-3.bam &
+
+samtools merge -f -h sh_202_1.bam \
+sh_202.bam \
+sh_202_1.bam \
+sh_202_2.bam \
+sh_202_3.bam &
+
+sh400-1.bam
+sh400-2.bam
+sh400-3.bam
+sh400.bam
+sh_202.bam
+sh_202_1.bam
+sh_202_2.bam
+sh_202_3.bam
+
+
+bamCoverage -p max -bs 20 --normalizeUsingRPKM -b sh400-1.bam \
+-o sh400-1.bw
+
+bamCoverage -p max -bs 20 --normalizeUsingRPKM -b sh400-2.bam \
+-o sh400-2.bw
+
+bamCoverage -p max -bs 20 --normalizeUsingRPKM -b sh400-3.bam \
+-o sh400-3.bw
+
+bamCoverage -p max -bs 20 --normalizeUsingRPKM -b sh400.bam \
+-o sh400.bw
+
+bamCoverage -p max -bs 20 --normalizeUsingRPKM -b sh202-1.bam \
+-o sh202-1.bw
+
+bamCoverage -p max -bs 20 --normalizeUsingRPKM -b sh202-1.bam \
+-o sh202-1.bw
+
+bamCoverage -p max -bs 20 --normalizeUsingRPKM -b sh202-1.bam \
+-o sh202-1.bw
+
+bamCoverage -p max -bs 20 --normalizeUsingRPKM -b sh202.bam \
+-o sh202.bw
