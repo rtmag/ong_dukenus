8cadbfe74129f27d83724de3fe45c5c7dea6d7c3
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-03-14 19:50:19 +0800

    Update bw_creation.sh

--- bw_creation.sh
+++ bw_creation.sh
@@ -24,38 +24,38 @@ samtools sort sh_202_3.bam > sh_202_3_sort.bam &
 
 ######
 
-samtools index sh400-1.bam &
-samtools index sh400-2.bam &
-samtools index sh400-3.bam &
-samtools index sh400.bam &
-samtools index sh202-1.bam &
-samtools index sh202-2.bam &
-samtools index sh202-3.bam &
-samtools index sh202.bam &
+samtools index sh400-1_sort.bam &
+samtools index sh400-2_sort.bam &
+samtools index sh400-3_sort.bam &
+samtools index sh400_sort.bam &
+samtools index sh_202_1_sort.bam &
+samtools index sh_202_2_sort.bam &
+samtools index sh_202_3_sort.bam &
+samtools index sh_202_sort.bam &
 
 
 #####
 
-bamCoverage -p 10 -bs 1 --normalizeUsingRPKM -b sh400-1.bam \
+bamCoverage -p 10 -bs 1 --normalizeUsingRPKM -b sh400-1_sort.bam \
 -o sh400-1.bw
 
-bamCoverage -p 10 -bs 1 --normalizeUsingRPKM -b sh400-2.bam \
+bamCoverage -p 10 -bs 1 --normalizeUsingRPKM -b sh400-2_sort.bam \
 -o sh400-2.bw
 
-bamCoverage -p 10 -bs 1 --normalizeUsingRPKM -b sh400-3.bam \
+bamCoverage -p 10 -bs 1 --normalizeUsingRPKM -b sh400-3_sort.bam  \
 -o sh400-3.bw
 
-bamCoverage -p 10 -bs 1 --normalizeUsingRPKM -b sh202-1.bam \
--o sh202-1.bw
-
-bamCoverage -p 10 -bs 1 --normalizeUsingRPKM -b sh202-1.bam \
--o sh202-1.bw
-
-bamCoverage -p 10 -bs 1 --normalizeUsingRPKM -b sh202-1.bam \
--o sh202-1.bw
-
-bamCoverage -p 10 -bs 1 --normalizeUsingRPKM -b sh400.bam \
+bamCoverage -p 10 -bs 1 --normalizeUsingRPKM -b sh400_sort.bam \
 -o sh400.bw
 
-bamCoverage -p 10 -bs 1 --normalizeUsingRPKM -b sh202.bam \
+bamCoverage -p 10 -bs 1 --normalizeUsingRPKM -b sh_202_1_sort.bam \
+-o sh202-1.bw
+
+bamCoverage -p 10 -bs 1 --normalizeUsingRPKM -b sh_202_2_sort.bam \
+-o sh202-2.bw
+
+bamCoverage -p 10 -bs 1 --normalizeUsingRPKM -b sh_202_3_sort.bam \
+-o sh202-3.bw
+
+bamCoverage -p 10 -bs 1 --normalizeUsingRPKM -b sh_202_sort.bam \
 -o sh202.bw
