30d731a89f08497af00fa5345357bf5d482d009d
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-03-14 17:41:26 +0800

    Update bw_creation.sh

--- bw_creation.sh
+++ bw_creation.sh
@@ -10,36 +10,28 @@ sh_202_1.bam \
 sh_202_2.bam \
 sh_202_3.bam &
 
-sh400-1.bam
-sh400-2.bam
-sh400-3.bam
-sh400.bam
-sh_202.bam
-sh_202_1.bam
-sh_202_2.bam
-sh_202_3.bam
 
 
-bamCoverage -p max -bs 20 --normalizeUsingRPKM -b sh400-1.bam \
+bamCoverage -p 10 -bs 1 --normalizeUsingRPKM -b sh400-1.bam \
 -o sh400-1.bw
 
-bamCoverage -p max -bs 20 --normalizeUsingRPKM -b sh400-2.bam \
+bamCoverage -p 10 -bs 1 --normalizeUsingRPKM -b sh400-2.bam \
 -o sh400-2.bw
 
-bamCoverage -p max -bs 20 --normalizeUsingRPKM -b sh400-3.bam \
+bamCoverage -p 10 -bs 1 --normalizeUsingRPKM -b sh400-3.bam \
 -o sh400-3.bw
 
-bamCoverage -p max -bs 20 --normalizeUsingRPKM -b sh400.bam \
+bamCoverage -p 10 -bs 1 --normalizeUsingRPKM -b sh202-1.bam \
+-o sh202-1.bw
+
+bamCoverage -p 10 -bs 1 --normalizeUsingRPKM -b sh202-1.bam \
+-o sh202-1.bw
+
+bamCoverage -p 10 -bs 1 --normalizeUsingRPKM -b sh202-1.bam \
+-o sh202-1.bw
+
+bamCoverage -p 10 -bs 1 --normalizeUsingRPKM -b sh400.bam \
 -o sh400.bw
 
-bamCoverage -p max -bs 20 --normalizeUsingRPKM -b sh202-1.bam \
--o sh202-1.bw
-
-bamCoverage -p max -bs 20 --normalizeUsingRPKM -b sh202-1.bam \
--o sh202-1.bw
-
-bamCoverage -p max -bs 20 --normalizeUsingRPKM -b sh202-1.bam \
--o sh202-1.bw
-
-bamCoverage -p max -bs 20 --normalizeUsingRPKM -b sh202.bam \
+bamCoverage -p 10 -bs 1 --normalizeUsingRPKM -b sh202.bam \
 -o sh202.bw
