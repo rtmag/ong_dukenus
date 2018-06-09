f099c5555f20d548442b90c57ba61d4d91c19126
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-06-10 01:30:32 +0800

    Create chip-seq_preprossesing.bash

---
+++ chip-seq_preprossesing.bash
@@ -0,0 +1,39 @@
+trim_galore --illumina --paired -o /root/ong_dukenus/chip-seq/trimmed/ \
+c_1_1.fq.gz \
+c_1_2.fq.gz
+
+trim_galore --illumina --paired -q --fastqc -o /root/ong_dukenus/chip-seq/trimmed/ \
+/root/ong_dukenus/chip-seq/MNase-seqDATA_1st_batch/SH143_gDNA/FCHTM5YBBXX_L7_CHKPEI85218050078_1.fq.gz \
+/root/ong_dukenus/chip-seq/MNase-seqDATA_1st_batch/SH143_gDNA/FCHTM5YBBXX_L7_CHKPEI85218050078_2.fq.gz
+
+trim_galore --illumina --paired -q --fastqc -o /root/ong_dukenus/chip-seq/trimmed/ \
+/root/ong_dukenus/chip-seq/MNase-seqDATA_1st_batch/sh143_IP/FCHTM5YBBXX_L7_CHKPEI85218050075_1.fq.gz \
+/root/ong_dukenus/chip-seq/MNase-seqDATA_1st_batch/sh143_IP/FCHTM5YBBXX_L7_CHKPEI85218050075_2.fq.gz
+
+trim_galore --illumina --paired -q --fastqc -o /root/ong_dukenus/chip-seq/trimmed/ \
+/root/ong_dukenus/chip-seq/MNase-seqDATA_1st_batch/sh143_input/FCHTM5YBBXX_L7_CHKPEI85218050072_1.fq.gz \
+/root/ong_dukenus/chip-seq/MNase-seqDATA_1st_batch/sh143_input/FCHTM5YBBXX_L7_CHKPEI85218050072_2.fq.gz
+
+trim_galore --illumina --paired -q --fastqc -o /root/ong_dukenus/chip-seq/trimmed/ \
+/root/ong_dukenus/chip-seq/MNase-seqDATA_1st_batch/sh400-IP/FCHTM5YBBXX_L7_CHKPEI85218050076_1.fq.gz \
+/root/ong_dukenus/chip-seq/MNase-seqDATA_1st_batch/sh400-IP/FCHTM5YBBXX_L7_CHKPEI85218050076_2.fq.gz
+
+trim_galore --illumina --paired -q --fastqc -o /root/ong_dukenus/chip-seq/trimmed/ \
+/root/ong_dukenus/chip-seq/MNase-seqDATA_1st_batch/sh400-gDNA/FCHTM5YBBXX_L7_CHKPEI85218050079_1.fq.gz \
+/root/ong_dukenus/chip-seq/MNase-seqDATA_1st_batch/sh400-gDNA/FCHTM5YBBXX_L7_CHKPEI85218050079_2.fq.gz
+
+trim_galore --illumina --paired -q --fastqc -o /root/ong_dukenus/chip-seq/trimmed/ \
+/root/ong_dukenus/chip-seq/MNase-seqDATA_1st_batch/sh400-input/FCHTM5YBBXX_L7_CHKPEI85218050073_1.fq.gz \
+/root/ong_dukenus/chip-seq/MNase-seqDATA_1st_batch/sh400-input/FCHTM5YBBXX_L7_CHKPEI85218050073_2.fq.gz
+
+trim_galore --illumina --paired -q --fastqc -o /root/ong_dukenus/chip-seq/trimmed/ \
+/root/ong_dukenus/chip-seq/MNase-seqDATA_1st_batch/shNT-IP/FCHTM5YBBXX_L7_CHKPEI85218050074_1.fq.gz \
+/root/ong_dukenus/chip-seq/MNase-seqDATA_1st_batch/shNT-IP/FCHTM5YBBXX_L7_CHKPEI85218050074_2.fq.gz
+
+trim_galore --illumina --paired -q --fastqc -o /root/ong_dukenus/chip-seq/trimmed/ \
+/root/ong_dukenus/chip-seq/MNase-seqDATA_1st_batch/shNT-gDNA/FCHTM5YBBXX_L7_CHKPEI85218050077_1.fq.gz \
+/root/ong_dukenus/chip-seq/MNase-seqDATA_1st_batch/shNT-gDNA/FCHTM5YBBXX_L7_CHKPEI85218050077_2.fq.gz
+
+trim_galore --illumina --paired -q --fastqc -o /root/ong_dukenus/chip-seq/trimmed/ \
+/root/ong_dukenus/chip-seq/MNase-seqDATA_1st_batch/shNT-input/FCHTM5YBBXX_L7_CHKPEI85218050071_1.fq.gz \
+/root/ong_dukenus/chip-seq/MNase-seqDATA_1st_batch/shNT-input/FCHTM5YBBXX_L7_CHKPEI85218050071_2.fq.gz
