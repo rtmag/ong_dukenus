f0b662c1e6976ddd676c5271c70879b009e4a761
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-06-19 21:06:49 +0800

    Update heatmap_chipseq.bash

--- heatmap_chipseq.bash
+++ heatmap_chipseq.bash
@@ -31,3 +31,23 @@ computeMatrix reference-point \
 -R /root/resources/hg19_tss_knownCanonical_noUnasembled.bed --referencePoint center \
 --sortRegions descend -bs 20 -a 2000 -b 2000 -p 40 -out /root/ong_dukenus/chip-seq/heatmaps/chipseq_meta_tss_h19_allKnownCanonicalGenes.mat
 ###############################
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "ATAC Peak" --colorMap Blues \
+-m /root/ong_dukenus/chip-seq/heatmaps/chipseq_on_atac_diffreps_log2fc1_avg40.mat \
+ --samplesLabel "shH2AFV#1-rep1" "shH2AFV#2-rep1" "shNT-rep1" \
+-out /root/ong_dukenus/paul_heatmap/chipseq_on_atac_diffreps_log2fc1_avg40.pdf
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "ATAC Peak" --colorMap Blues \
+-m /root/ong_dukenus/chip-seq/heatmaps/chipseq_on_atac_diffreps_log2fc1_avg100.mat \
+ --samplesLabel "shH2AFV#1-rep1" "shH2AFV#2-rep1" "shNT-rep1" \
+-out /root/ong_dukenus/paul_heatmap/chipseq_on_atac_diffreps_log2fc1_avg100.pdf
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "H2AFV Peak" --colorMap Blues \
+-m /root/ong_dukenus/chip-seq/heatmaps/chipseq_comparison.mat \
+ --samplesLabel "shH2AFV#1-rep1" "shH2AFV#2-rep1" "shNT-rep1" \
+-out /root/ong_dukenus/paul_heatmap/chipseq_comparison.pdf
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "TSS" --colorMap Blues \
+-m /root/ong_dukenus/chip-seq/heatmaps/chipseq_meta_tss_h19_allKnownCanonicalGenes.mat \
+ --samplesLabel "shH2AFV#1-rep1" "shH2AFV#2-rep1" "shNT-rep1" \
+-out /root/ong_dukenus/paul_heatmap/chipseq_meta_tss_h19_allKnownCanonicalGenes.pdf
+######
