c2fb4b7055151021e1b2e256fec4b2ca1e92d95b
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-06-22 11:28:19 +0800

    Update chip-seq_preprossesing.bash

--- chip-seq_preprossesing.bash
+++ chip-seq_preprossesing.bash
@@ -382,10 +382,27 @@ computeMatrix reference-point \
 /root/ong_dukenus/chip-seq/bw/sh400-input_1.bw \
 /root/ong_dukenus/chip-seq/bw/shNT-input_1.bw \
 -R /root/resources/hg19_tss_knownCanonical_noUnasembled.bed --referencePoint center \
---sortRegions descend -bs 20 -a 2000 -b 2000 -p max -out mnaseseq.mat
+--sortRegions descend -bs 20 -a 1000 -b 1000 -p max -out mnaseseq.mat
 
 
 plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "TSS" --colorMap Blues \
 -m mnaseseq.mat --regionsLabel "genes" \
  --samplesLabel "sh143" "sh400" "shNT" \
--out mnaseseq.pdf
+-out mnaseseq20.pdf
+
+computeMatrix reference-point \
+-S \
+/root/ong_dukenus/chip-seq/bw/sh143_input_1.bw \
+/root/ong_dukenus/chip-seq/bw/sh400-input_1.bw \
+/root/ong_dukenus/chip-seq/bw/shNT-input_1.bw \
+-R /root/resources/hg19_tss_knownCanonical_noUnasembled.bed --referencePoint center \
+--sortRegions descend -bs 1 -a 1000 -b 1000 -p max -out mnaseseq.mat
+
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "TSS" --colorMap Blues \
+-m mnaseseq.mat --regionsLabel "genes" \
+ --samplesLabel "sh143" "sh400" "shNT" \
+-out mnaseseq1.pdf
+###########
+###
+###
