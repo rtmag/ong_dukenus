0b35d7f79293fcefafcefc4813ce59f067f05a54
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-06-29 13:48:01 +0800

    Update atac-seq_preprocessing.bash

--- atac-seq/atac-seq_preprocessing.bash
+++ atac-seq/atac-seq_preprocessing.bash
@@ -37,8 +37,8 @@ STAR --genomeDir /root/resources/hg19_noanno/ \
 --outFilterMultimapNmax 1 \
 --alignEndsType EndToEnd \
 --readFilesIn \
-/root/ong_dukenus/ATAC-SEQ/trimmed/1_3502DukeNus_TS543-NT-031117_hs_i9_r1_val_1.fastq.gz \
-/root/ong_dukenus/ATAC-SEQ/trimmed/1_3502DukeNus_TS543-NT-031117_hs_i9_r2_val_2.fastq.gz \
+/root/ong_dukenus/ATAC-SEQ/trimmed/1_3502DukeNus_TS543-NT-031117_hs_i9_r1_val_1.fq.gz \
+/root/ong_dukenus/ATAC-SEQ/trimmed/1_3502DukeNus_TS543-NT-031117_hs_i9_r2_val_2.fq.gz \
 --outSAMtype BAM SortedByCoordinate \
 --outFileNamePrefix /root/ong_dukenus/ATAC-SEQ/bam/shNT_1_
 
@@ -51,8 +51,8 @@ STAR --genomeDir /root/resources/hg19_noanno/ \
 --outFilterMultimapNmax 1 \
 --alignEndsType EndToEnd \
 --readFilesIn \
-/root/ong_dukenus/ATAC-SEQ/trimmed/2_3502DukeNus_TS543-143-031117_hs_i10_r1_val_1.fastq.gz \
-/root/ong_dukenus/ATAC-SEQ/trimmed/2_3502DukeNus_TS543-143-031117_hs_i10_r2_val_2.fastq.gz \
+/root/ong_dukenus/ATAC-SEQ/trimmed/2_3502DukeNus_TS543-143-031117_hs_i10_r1_val_1.fq.gz \
+/root/ong_dukenus/ATAC-SEQ/trimmed/2_3502DukeNus_TS543-143-031117_hs_i10_r2_val_2.fq.gz \
 --outSAMtype BAM SortedByCoordinate \
 --outFileNamePrefix /root/ong_dukenus/ATAC-SEQ/bam/shH2_I_1_
 
@@ -65,8 +65,8 @@ STAR --genomeDir /root/resources/hg19_noanno/ \
 --outFilterMultimapNmax 1 \
 --alignEndsType EndToEnd \
 --readFilesIn \
-/root/ong_dukenus/ATAC-SEQ/trimmed/3_3502DukeNus_TS543-400-031117_hs_i11_r1_val_1.fastq.gz \
-/root/ong_dukenus/ATAC-SEQ/trimmed/3_3502DukeNus_TS543-400-031117_hs_i11_r2_val_2.fastq.gz \
+/root/ong_dukenus/ATAC-SEQ/trimmed/3_3502DukeNus_TS543-400-031117_hs_i11_r1_val_1.fq.gz \
+/root/ong_dukenus/ATAC-SEQ/trimmed/3_3502DukeNus_TS543-400-031117_hs_i11_r2_val_2.fq.gz \
 --outSAMtype BAM SortedByCoordinate \
 --outFileNamePrefix /root/ong_dukenus/ATAC-SEQ/bam/shH2_II_1_
 
@@ -79,8 +79,8 @@ STAR --genomeDir /root/resources/hg19_noanno/ \
 --outFilterMultimapNmax 1 \
 --alignEndsType EndToEnd \
 --readFilesIn \
-/root/ong_dukenus/ATAC-SEQ/trimmed/4_3502DukeNus_TS543-NT-241117_hs_i12_r1_val_1.fastq.gz \
-/root/ong_dukenus/ATAC-SEQ/trimmed/4_3502DukeNus_TS543-NT-241117_hs_i12_r2_val_2.fastq.gz \
+/root/ong_dukenus/ATAC-SEQ/trimmed/4_3502DukeNus_TS543-NT-241117_hs_i12_r1_val_1.fq.gz \
+/root/ong_dukenus/ATAC-SEQ/trimmed/4_3502DukeNus_TS543-NT-241117_hs_i12_r2_val_2.fq.gz \
 --outSAMtype BAM SortedByCoordinate \
 --outFileNamePrefix /root/ong_dukenus/ATAC-SEQ/bam/shNT_2_
 
@@ -93,8 +93,8 @@ STAR --genomeDir /root/resources/hg19_noanno/ \
 --outFilterMultimapNmax 1 \
 --alignEndsType EndToEnd \
 --readFilesIn \
-/root/ong_dukenus/ATAC-SEQ/trimmed/5_3502DukeNus_TS543-143-241117_hs_i13_r1_val_1.fastq.gz \
-/root/ong_dukenus/ATAC-SEQ/trimmed/5_3502DukeNus_TS543-143-241117_hs_i13_r2_val_2.fastq.gz \
+/root/ong_dukenus/ATAC-SEQ/trimmed/5_3502DukeNus_TS543-143-241117_hs_i13_r1_val_1.fq.gz \
+/root/ong_dukenus/ATAC-SEQ/trimmed/5_3502DukeNus_TS543-143-241117_hs_i13_r2_val_2.fq.gz \
 --outSAMtype BAM SortedByCoordinate \
 --outFileNamePrefix /root/ong_dukenus/ATAC-SEQ/bam/shH2_II_2_
 
@@ -107,8 +107,8 @@ STAR --genomeDir /root/resources/hg19_noanno/ \
 --outFilterMultimapNmax 1 \
 --alignEndsType EndToEnd \
 --readFilesIn \
-/root/ong_dukenus/ATAC-SEQ/trimmed/6_3502DukeNus_TS543-400-241117_hs_i14_r1_val_1.fastq.gz \
-/root/ong_dukenus/ATAC-SEQ/trimmed/6_3502DukeNus_TS543-400-241117_hs_i14_r2_val_2.fastq.gz \
+/root/ong_dukenus/ATAC-SEQ/trimmed/6_3502DukeNus_TS543-400-241117_hs_i14_r1_val_1.fq.gz \
+/root/ong_dukenus/ATAC-SEQ/trimmed/6_3502DukeNus_TS543-400-241117_hs_i14_r2_val_2.fq.gz \
 --outSAMtype BAM SortedByCoordinate \
 --outFileNamePrefix /root/ong_dukenus/ATAC-SEQ/bam/shH2_II_2_
 #########################################################################################
