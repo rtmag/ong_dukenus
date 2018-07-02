8747d0bddc61e153160c8306162717011cbfa2e4
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-07-03 00:14:16 +0800

    Update deseq2_atac.R

--- atac-seq/deseq2_atac.R
+++ atac-seq/deseq2_atac.R
@@ -22,10 +22,32 @@ countData=fc_SE$counts
 colnames(countData)=c ( "shH2_I_1","shH2_I_2","shH2_II_1","shH2_II_2","shNT_1","shNT_2" )
 
 saveRDS(countData,'atac_countdata.rds')
-##
-#
+######################################################################################
+
 
 countData=readRDS('atac_countdata.rds')
+require(DESeq2)
+colData <- data.frame(group=c ( "shH2_I_1","shH2_I_2","shH2_II_1","shH2_II_2","shNT_1","shNT_2" ) )
+dds <- DESeqDataSetFromMatrix(
+       countData = countData,
+       colData = colData,
+design = ~ group)
+#####################################################################################
 
+dLRT <- DESeq(dds, test="LRT", reduced=~1)
+dLRT_vsd <- varianceStabilizingTransformation(dLRT)
+dLRT_res <- results(dLRT)
+dLRT_res$padj[is.na(dLRT_res$padj)]=1
 
-#
+pdf("Diagnostic_design_pca.pdf")
+plotPCA(dLRT_vsd,ntop=90000,intgroup=c('group'))
+dev.off()
+#####################################################################################
+
+design<-data.frame(cells = c ( "shH2","shH2","shH2","shH2","shNT","shNT" ) )
+dds <- DESeqDataSetFromMatrix(countData = countData, colData = design, design = ~ cells)
+dds <- DESeq(dds)
+res <- results(dds, contrast=c("cells","shH2","shNT"))
+write.csv(res,"shH2_vs_shNT.csv")
+
+#####################################################################################
