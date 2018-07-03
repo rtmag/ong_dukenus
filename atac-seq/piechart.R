8bca131fd79d9965e3be210ba0413af949751857
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-07-03 13:03:03 +0800

    Update piechart.R

--- atac-seq/piechart.R
+++ atac-seq/piechart.R
@@ -1,14 +1,14 @@
-#annotatePeaks.pl highconfidence.broadpeak hg19 -annStats highconfidence.annStats > highconfidence.bed.anno
+#annotatePeaks.pl h2_vs_nt_100reads_down.bed hg19 -annStats h2_vs_nt_100reads_down.annStats > h2_vs_nt_100reads_down.bed.anno
 #############################################################################################
-pdf("shNT_h2afv_highconfidence.pdf")
-res=read.table(pipe("more highconfidence.annStats |cut -f1,2,4"), sep="\t",header=F)
+pdf("h2_vs_nt_100reads_down.pdf")
+res=read.table(pipe("more h2_vs_nt_100reads_down.annStats |cut -f1,2,4"), sep="\t",header=F)
 i1 = which(res[,1]=="Annotation")[2]+1
 i2 = dim(res)[1]
 res = res[ i1:i2,]
 tdown = as.numeric(as.character(res[,2]))
 names(tdown) = res[,1]
 names(tdown) = paste(names(tdown)," ",round(tdown/sum(tdown)*100,digits=2),"%",sep="")
-tdown = tdown[tdown>200]
+tdown = tdown[tdown>50]
 pie(sort(tdown), main=,cex=.8)
-title("Distribution of H2AFV peaks\n(41,294 regions)", cex.main=.9)
+title("Distribution of open chromatin in shNT\n(5,584 regions)", cex.main=.9)
 dev.off()
