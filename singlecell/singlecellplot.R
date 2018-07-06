9c71a845b0da833091cbbb73effeafba57da0bc1
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-07-06 16:37:46 +0800

    Update singlecellplot.R

--- singlecell/singlecellplot.R
+++ singlecell/singlecellplot.R
@@ -36,22 +36,22 @@ colnames(tpm)[colnames(tpm)=="BC11"] = "TNBC #5"
 
 ############################################################################################################
 H2AFV = tpm[rownames(tpm)=="H2AFV",]
-H2AFV = data.frame(cell = colnames(tpm), gene=as.numeric(H2AFV))
+H2AFV = data.frame(cell = colnames(tpm), gene=log2(as.numeric(H2AFV)+1) )
 
 
 H2AFZ = tpm[rownames(tpm)=="H2AFZ",]
-H2AFZ = data.frame(cell = colnames(tpm), gene=as.numeric(H2AFZ))
+H2AFZ = data.frame(cell = colnames(tpm), gene=log2(as.numeric(H2AFZ)+1) )
 
 ############################################################################################################
 
-pdf("breast_primary_tumor.pdf",width=14)
+pdf("breast_primary_tumor_log2.pdf",width=14)
 
 par(mfrow = c(2,1) )
 
-stripchart(gene ~ cell, vertical = TRUE, data = H2AFV, jitter = 0.3, ylab = "Single Cell H2AFV TPM",
+stripchart(gene ~ cell, vertical = TRUE, data = H2AFV, jitter = 0.3, ylab = expression('Single Cell H2AFV log'[2]*' TPM'),
     method = "jitter", pch = 20, col = alpha(colour='red',alpha=.5),cex = 2)
 
-stripchart(gene ~ cell, vertical = TRUE, data = H2AFZ, jitter = 0.3, ylab = "Single Cell H2AFZ TPM",
+stripchart(gene ~ cell, vertical = TRUE, data = H2AFZ, jitter = 0.3, ylab = expression('Single Cell H2AFV log'[2]*' TPM'),
     method = "jitter", pch = 20, col = alpha(colour='red',alpha=.5),cex = 2)
 
 dev.off()
