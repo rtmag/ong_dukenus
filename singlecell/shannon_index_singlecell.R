00ee4fa4a4f87ae1f49d4bea764004d7c90828f5
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-07-11 17:58:02 +0800

    Update shannon_index_singlecell.R

--- singlecell/shannon_index_singlecell.R
+++ singlecell/shannon_index_singlecell.R
@@ -1,12 +1,73 @@
-# GBM
+# GBM 1 - 5 #OLIG2 - cancer stem cell marker
 
 
 
-# MGH29
+# MGH's
 
-
+#################################################################################################################################
 # BREAST #ITG86 - cancer stem cell marker
 
+data = read.table("GSE75688_GEO_processed_Breast_Cancer_raw_TPM_matrix.txt",sep="\t",header=T)
+
+# BC01-BC02, estrogen receptor positive (ER+); 
+# BC03, double positive (ER+ and HER2+); 
+# BC04-BC06, human epidermal growth factor receptor 2 positive (HER2+); 
+# BC07-BC11, triple-negative breast cancer (TNBC); 
+# BC03LN, lymph node metastasis of BC03; 
+# BC07LN, lymph node metastasis of BC07)
+library(gplots)
+library(ggplot2)
+library(graphics)
+library(scales)
+
+tpm = data[,18:dim(data)[2]]
+rownames(tpm) = make.names(data[,2],unique=T)
+colnames(tpm) = gsub("\\_.+","",colnames(tpm),perl=T)
+
+#remove LN
+tpm = tpm[,grep("LN",colnames(tpm),invert=T)]
+colnames(tpm) = gsub("\\..+","",colnames(tpm),perl=T)
+
+
+colnames(tpm)[colnames(tpm)=="BC01"] = "ER+ #1"
+colnames(tpm)[colnames(tpm)=="BC02"] = "ER+ #2"
+colnames(tpm)[colnames(tpm)=="BC03"] = "ER+_HER2+"
+colnames(tpm)[colnames(tpm)=="BC03LN"] = "ER+_HER2+_LN"
+colnames(tpm)[colnames(tpm)=="BC04"] = "HER2+ #1"
+colnames(tpm)[colnames(tpm)=="BC05"] = "HER2+ #2"
+colnames(tpm)[colnames(tpm)=="BC06"] = "HER2+ #3"
+colnames(tpm)[colnames(tpm)=="BC07"] = "TNBC #1"
+colnames(tpm)[colnames(tpm)=="BC07LN"] = "TNBC_LN"
+colnames(tpm)[colnames(tpm)=="BC08"] = "TNBC #2"
+colnames(tpm)[colnames(tpm)=="BC09"] = "TNBC #3"
+colnames(tpm)[colnames(tpm)=="BC10"] = "TNBC #4"
+colnames(tpm)[colnames(tpm)=="BC11"] = "TNBC #5"
+
+H2AFV = tpm[rownames(tpm)=="H2AFV",]
+H2AFV = data.frame(cell = colnames(tpm), gene=(as.numeric(H2AFV)+1) )
+
+
+H2AFZ = tpm[rownames(tpm)=="H2AFZ",]
+H2AFZ = data.frame(cell = colnames(tpm), gene=(as.numeric(H2AFZ)+1) )
+
+ITGA6 = tpm[rownames(tpm)=="ITGA6",]
+ITGA6 = data.frame(cell = colnames(tpm), gene=(as.numeric(ITGA6)+1) )
+
+
+par(mfrow = c(3,1) )
+
+stripchart(gene ~ cell, vertical = TRUE, data = H2AFV, jitter = 0.3, ylab = expression('Single Cell H2AFV TPM'),
+    method = "jitter", pch = 20, col = alpha(colour='red',alpha=.5),cex = 2)
+
+stripchart(gene ~ cell, vertical = TRUE, data = H2AFZ, jitter = 0.3, ylab = expression('Single Cell H2AFV TPM'),
+    method = "jitter", pch = 20, col = alpha(colour='red',alpha=.5),cex = 2)
+
+stripchart(gene ~ cell, vertical = TRUE, data = ITGA6, jitter = 0.3, ylab = expression('Single Cell ITGA6 TPM'),
+    method = "jitter", pch = 20, col = alpha(colour='red',alpha=.5),cex = 2)
+
+diversity(x, index = "shannon", MARGIN = 1, base = exp(1))
+
+#################################################################################################################################
 
 # MELANOMA
 
