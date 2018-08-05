6dec9e75a4cf44652a47d131c881262793d0f0db
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-08-06 03:39:29 +0800

    Update chip-atac.bash

--- chip-seq/chip-atac.bash
+++ chip-seq/chip-atac.bash
@@ -1,16 +1,34 @@
 
 computeMatrix reference-point \
 -S \
-/root/ong_dukenus/chip-seq/bw/shNT-IP_1.bw \
-/root/ong_dukenus/mnase_batch2/bw/sh143_IP_2.bw \
 /root/ong_dukenus/chip-seq/bw/sh143_IP_1.bw \
-/root/ong_dukenus/mnase_batch2/bw/sh400_IP_2.bw \
+/root/ong_dukenus/mnase_batch2/bw/sh143_IP_2.bw \
 /root/ong_dukenus/chip-seq/bw/sh400-IP_1.bw \
+/root/ong_dukenus/mnase_batch2/bw/sh400_IP_2.bw \
+/root/ong_dukenus/chip-seq/bw/shNT-IP_1.bw \
 /root/ong_dukenus/mnase_batch2/bw/shNT_IP_2.bw \
 -R /root/ong_dukenus/ATAC-SEQ/heatmap/h2_vs_nt_100reads.bed --referencePoint center \
---sortRegions descend --sortUsingSamples 1 -bs 20 -a 2000 -b 2000 -p 40 -out chip_atac.mat
+--sortRegions descend --sortUsingSamples 5 -bs 20 -a 2000 -b 2000 -p 40 -out chip_atac.mat
 
 plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "ATAC" --colorMap Blues \
 -m chip_atac.mat \
  --samplesLabel "shH2AFV I" "shH2AFV I" "shH2AFV II" "shH2AFV II" "shNT" "shNT" \
 -out chip_atac.pdf
+
+
+
+computeMatrix reference-point \
+-S \
+/root/ong_dukenus/chip-seq/bw/sh143_IP_1.bw \
+/root/ong_dukenus/mnase_batch2/bw/sh143_IP_2.bw \
+/root/ong_dukenus/chip-seq/bw/sh400-IP_1.bw \
+/root/ong_dukenus/mnase_batch2/bw/sh400_IP_2.bw \
+/root/ong_dukenus/chip-seq/bw/shNT-IP_1.bw \
+/root/ong_dukenus/mnase_batch2/bw/shNT_IP_2.bw \
+-R /root/ong_dukenus/mnase_analysis/ATAC_shNT_only.bed --referencePoint center \
+--sortRegions descend --sortUsingSamples 5 -bs 20 -a 2000 -b 2000 -p 40 -out chip_atac_shNT.mat
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "ATAC" --colorMap Blues \
+-m chip_atac_shNT.mat \
+ --samplesLabel "shH2AFV I" "shH2AFV I" "shH2AFV II" "shH2AFV II" "shNT" "shNT" \
+-out chip_atac_shNT.pdf
