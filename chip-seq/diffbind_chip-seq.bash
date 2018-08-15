53a552c348d6162cfb46cc9746ce88cf258efbc2
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-08-15 19:21:07 +0800

    Update diffbind_chip-seq.bash

--- chip-seq/diffbind_chip-seq.bash
+++ chip-seq/diffbind_chip-seq.bash
@@ -36,3 +36,43 @@ diffReps.pl --treatment ./bed/sh143_IP_2_rmdup.bed \
 ./bed/sh400_IP_2_rmdup.bed \
 --control ./bed/shNT_IP_2_rmdup.bed \
 --meth gt --gname hg19 --report /root/ong_dukenus/chip-DIFF/diffreps/diffChip_batch2 --frag 0 --nproc 30 &
+##################v
+more ../diffreps/diffChip_both|grep -v "#"|grep -w Up |cut -f1,2,3 > diffboth.bed
+echo "#shH2AFV-up" >> diffboth.bed
+more ../diffreps/diffChip_both|grep -v "#"|grep -w Down |cut -f1,2,3 >> diffboth.bed
+echo "#shNT-up" >> diffboth.bed
+ 
+more ../diffreps/diffChip_batch2|grep -v "#"|grep -w Up |cut -f1,2,3 > diffbatch2.bed
+echo "#shH2AFV-up" >> diffbatch2.bed
+more ../diffreps/diffChip_batch2|grep -v "#"|grep -w Down |cut -f1,2,3 >> diffbatch2.bed
+echo "#shNT-up" >> diffbatch2.bed
+
+computeMatrix reference-point -S \
+/root/ong_dukenus/chip-seq/bw/sh143_IP_1.bw \
+/root/ong_dukenus/mnase_batch2/bw/sh143_IP_2.bw \
+/root/ong_dukenus/chip-seq/bw/sh400-IP_1.bw \
+/root/ong_dukenus/mnase_batch2/bw/sh400_IP_2.bw \
+/root/ong_dukenus/chip-seq/bw/shNT-IP_1.bw \
+/root/ong_dukenus/mnase_batch2/bw/shNT_IP_2.bw \
+-R diffboth.bed --referencePoint center \
+--sortRegions descend --sortUsingSamples 5 -bs 20 -a 2000 -b 2000 -p max -out diffboth.mat
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "ATAC" --colorMap Blues \
+-m diffboth.mat \
+ --samplesLabel "shH2AFV I" "shH2AFV I" "shH2AFV II" "shH2AFV II" "shNT" "shNT" \
+-out diffboth.pdf
+
+computeMatrix reference-point -S \
+/root/ong_dukenus/chip-seq/bw/sh143_IP_1.bw \
+/root/ong_dukenus/mnase_batch2/bw/sh143_IP_2.bw \
+/root/ong_dukenus/chip-seq/bw/sh400-IP_1.bw \
+/root/ong_dukenus/mnase_batch2/bw/sh400_IP_2.bw \
+/root/ong_dukenus/chip-seq/bw/shNT-IP_1.bw \
+/root/ong_dukenus/mnase_batch2/bw/shNT_IP_2.bw \
+-R diffbatch2.bed --referencePoint center \
+--sortRegions descend --sortUsingSamples 5 -bs 20 -a 2000 -b 2000 -p max -out diffbatch2.mat
+
+plotHeatmap --xAxisLabel "" --yAxisLabel "" --refPointLabel "ATAC" --colorMap Blues \
+-m diffbatch2.mat \
+ --samplesLabel "shH2AFV I" "shH2AFV I" "shH2AFV II" "shH2AFV II" "shNT" "shNT" \
+-out diffbatch2.pdf
