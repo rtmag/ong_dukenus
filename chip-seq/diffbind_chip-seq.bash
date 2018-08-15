11349ac3541767a81741cdc068c67e6424e0bbe1
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-08-15 15:36:30 +0800

    Update diffbind_chip-seq.bash

--- chip-seq/diffbind_chip-seq.bash
+++ chip-seq/diffbind_chip-seq.bash
@@ -18,3 +18,21 @@ macs2 callpeak -f BAMPE -g hs -q 0.00001 --broad --keep-dup auto -n sh400_1 --ou
 macs2 callpeak -f BAMPE -g hs -q 0.00001 --broad --keep-dup auto -n sh400_2 --outdir /root/ong_dukenus/chip-DIFF/macs/ \
 -t /root/ong_dukenus/mnase_batch2/bam/sh400_IP_2_rmdup.bam -c /root/ong_dukenus/mnase_batch2/bam/sh400_mnase_2_rmdup.bam &
 #
+# DIFFREPS
+bamToBed -i /root/ong_dukenus/chip-seq/bam/shNT-IP_1_rmdup.bam > /root/ong_dukenus/chip-DIFF/bed/shNT_IP_1_rmdup.bed &
+bamToBed -i /root/ong_dukenus/mnase_batch2/bam/shNT_IP_2_rmdup.bam > /root/ong_dukenus/chip-DIFF/bed/shNT_IP_2_rmdup.bed &
+bamToBed -i /root/ong_dukenus/chip-seq/bam/sh143_IP_1_rmdup.bam > /root/ong_dukenus/chip-DIFF/bed/sh143_IP_1_rmdup.bed &
+bamToBed -i /root/ong_dukenus/mnase_batch2/bam/sh143_IP_2_rmdup.bam > /root/ong_dukenus/chip-DIFF/bed/sh143_IP_2_rmdup.bed &
+bamToBed -i /root/ong_dukenus/chip-seq/bam/sh400-IP_1_rmdup.bam > /root/ong_dukenus/chip-DIFF/bed/sh400_IP_1_rmdup.bed &
+bamToBed -i /root/ong_dukenus/mnase_batch2/bam/sh400_IP_2_rmdup.bam > /root/ong_dukenus/chip-DIFF/bed/sh400_IP_2_rmdup.bed &
+
+
+diffReps.pl --treatment ./bed/sh143_IP_1_rmdup.bed ./bed/sh143_IP_2_rmdup.bed \
+./bed/sh400_IP_1_rmdup.bed ./bed/sh400_IP_2_rmdup.bed \
+--control ./bed/shNT_IP_1_rmdup.bed ./bed/shNT_IP_2_rmdup.bed \
+--meth nb --gname hg19 --report /root/ong_dukenus/chip-DIFF/diffreps/diffChip_both --frag 0 --nproc 30 &
+
+diffReps.pl --treatment ./bed/sh143_IP_2_rmdup.bed \
+./bed/sh400_IP_2_rmdup.bed \
+--control ./bed/shNT_IP_2_rmdup.bed \
+--meth gt --gname hg19 --report /root/ong_dukenus/chip-DIFF/diffreps/diffChip_batch2 --frag 0 --nproc 30 &
