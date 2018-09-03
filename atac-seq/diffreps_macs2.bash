2a637928b1d44fb97a017f741f4b286027e5b456
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-09-03 12:23:09 +0800

    Update diffreps_macs2.bash

--- atac-seq/diffreps_macs2.bash
+++ atac-seq/diffreps_macs2.bash
@@ -7,9 +7,10 @@ mv atac_diffreps_macs2.tmp atac_diffreps_macs2.bed
 
 #########!R
 x=read.table("atac_diffreps_macs2.bed",sep="\t",header=T,stringsAsFactors=F)
+ix = (x[,7]>100|x[,8]>100) & (abs(x[,12])>0.66) & (x[,14]<0.05)
+filtered= x[ix,]
 
 
-
-
+table(filtered[,11])
 
 #########!R
