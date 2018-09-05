ed5289bf2247c6d3a97eb2108075cfd04ef8fe77
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-09-05 17:23:02 +0800

    Update atac_FILTERS.R

--- atac-seq/atac_FILTERS.R
+++ atac-seq/atac_FILTERS.R
@@ -8,8 +8,8 @@ x = x[(x$Control.avg>50 | x$Treatment.avg>50) & abs(x$log2FC)>.5,]
 write.table( x[x[,11]=="Up",1:3], "atac_diffreps_w100_FDR5_50R_l2FC5_Up.bed", sep="\t",quote=F,col.names=F,row.names=F)
 write.table( x[x[,11]=="Down",1:3], "atac_diffreps_w100_FDR5_50R_l2FC5_Down.bed", sep="\t",quote=F,col.names=F,row.names=F)
 
-write.table( x[x[,11]=="Up",1:3], "atac_diffreps_w100_FDR5_50R_l2FC5_Up_FullStats.txt", sep="\t",quote=F,col.names=T,row.names=F)
-write.table( x[x[,11]=="Down",1:3], "atac_diffreps_w100_FDR5_50R_l2FC5_Down_FullStats.txt", sep="\t",quote=F,col.names=T,row.names=F)
+write.table( x[x[,11]=="Up",], "atac_diffreps_w100_FDR5_50R_l2FC5_Up_FullStats.txt", sep="\t",quote=F,col.names=T,row.names=F)
+write.table( x[x[,11]=="Down",], "atac_diffreps_w100_FDR5_50R_l2FC5_Down_FullStats.txt", sep="\t",quote=F,col.names=T,row.names=F)
 ##!R
 ##!SHELL
 cat atac_diffreps_w100_FDR5_50R_l2FC5_Up.bed > /root/ong_dukenus/ATAC-SEQ/heatmap/atac_diffreps_w100_FDR5_50R_l2FC5.bed
