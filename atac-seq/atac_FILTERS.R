6a9c6d4f8a0e8280c2b61f1c438d7ddeb77d2de8
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-09-05 17:33:15 +0800

    Update atac_FILTERS.R

--- atac-seq/atac_FILTERS.R
+++ atac-seq/atac_FILTERS.R
@@ -37,5 +37,42 @@ pie(sort(tdown), main=,cex=.8)
 dev.off()
 
 pdf("ATAC-down_log2.pdf")
+par(mar=c(11.1,4.1,4.1,2))
+res=read.table(pipe("more ATAC-down.annStats |cut -f1,2,4"), sep="\t",header=F)
+i1 = which(res[,1]=="Annotation")[2]+1
+i2 = dim(res)[1]
+res = res[ i1:i2,]
+tdown = as.numeric(as.character(res[,3]))
+names(tdown) = res[,1]
+tdown = tdown[as.numeric(as.character(res[,2]))>50]
+barplot(sort(tdown),las=2,ylim=c(-4,6),ylab="Log2 Enrichment over random genomic background",col="lightblue3")
+abline(h=0)
+dev.off()
+##!R
 
+##!R
+pdf("ATAC-up_pie.pdf")
+res=read.table(pipe("more ATAC-up.annStats |cut -f1,2,4"), sep="\t",header=F)
+i1 = which(res[,1]=="Annotation")[2]+1
+i2 = dim(res)[1]
+res = res[ i1:i2,]
+tdown = as.numeric(as.character(res[,2]))
+names(tdown) = res[,1]
+names(tdown) = paste(names(tdown)," ",round(tdown/sum(tdown)*100,digits=2),"%",sep="")
+tdown = tdown[tdown>50]
+pie(sort(tdown), main=,cex=.8)
+dev.off()
+
+pdf("ATAC-up_log2.pdf")
+par(mar=c(11.1,4.1,4.1,2))
+res=read.table(pipe("more ATAC-up.annStats |cut -f1,2,4"), sep="\t",header=F)
+i1 = which(res[,1]=="Annotation")[2]+1
+i2 = dim(res)[1]
+res = res[ i1:i2,]
+tdown = as.numeric(as.character(res[,3]))
+names(tdown) = res[,1]
+tdown = tdown[as.numeric(as.character(res[,2]))>50]
+barplot(sort(tdown),las=2,ylim=c(-4,6),ylab="Log2 Enrichment over random genomic background",col="lightblue3")
+abline(h=0)
+dev.off()
 ##!R
