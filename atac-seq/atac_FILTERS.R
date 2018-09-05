50bc96fef0e0626c5628ceb83f30e3c0e5ebeb39
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-09-05 12:33:25 +0800

    Update atac_FILTERS.R

--- atac-seq/atac_FILTERS.R
+++ atac-seq/atac_FILTERS.R
@@ -3,6 +3,11 @@ more atac_diffreps_w100|grep -v "#"|grep -v "Treatment.avg"|awk -F"\t" '{if($14<
 ##!R
 x = read.table("atac_diffreps_w100_FDR.bed",sep="\t",header=T)
 x = x[(x$Control.avg>50 | x$Treatment.avg>50) & abs(x$log2FC)>.5,] 
-write.table( x[,1:3], "atac_diffreps_w100_FDR5_50R_l2FC5.bed", sep="\t",quote=F,col.names=F,row.names=F)
+write.table( x[x[,11]=="Up",1:3], "atac_diffreps_w100_FDR5_50R_l2FC5_Up.bed", sep="\t",quote=F,col.names=F,row.names=F)
+write.table( x[x[,11]=="Down",1:3], "atac_diffreps_w100_FDR5_50R_l2FC5_Down.bed", sep="\t",quote=F,col.names=F,row.names=F)
 ##!R
+cat atac_diffreps_w100_FDR5_50R_l2FC5_Up.bed > /root/ong_dukenus/ATAC-SEQ/heatmap/atac_diffreps_w100_FDR5_50R_l2FC5.bed
+echo "#shH2AFV specific" >> /root/ong_dukenus/ATAC-SEQ/heatmap/atac_diffreps_w100_FDR5_50R_l2FC5.bed
+cat atac_diffreps_w100_FDR5_50R_l2FC5_Down.bed |cut -f1-3 >> /root/ong_dukenus/ATAC-SEQ/heatmap/atac_diffreps_w100_FDR5_50R_l2FC5.bed
+echo "#shNT specific" >> /root/ong_dukenus/ATAC-SEQ/heatmap/atac_diffreps_w100_FDR5_50R_l2FC5.bed
 
