123253b6c4e89f188670a7f3bb9c34dcb985c4cf
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-09-19 01:50:49 +0800

    Update oleg_rnaseq_volcanoPlot.R

--- oleg_rnaseq_volcanoPlot.R
+++ oleg_rnaseq_volcanoPlot.R
@@ -23,3 +23,42 @@ points(dds_res$log2FoldChange[abs(dds_res$log2FoldChange)>.6 & dds_res$padj<0.05
 legend("topright", paste("shNT:",length(which(dds_res$log2FoldChange>.6 & dds_res$padj<0.05))), bty="n") 
 legend("topleft", paste("shH2AFV:",length(which(dds_res$log2FoldChange<(-.6) & dds_res$padj<0.05))), bty="n") 
 dev.off()
+########
+#REDO .37
+x = read.csv("Master_tab_o_202_143_400_R.csv")
+data=x[,c('PPEE','PostFC','X')]
+data = data[data$X!="N/A",]
+data = data[complete.cases(data),]
+data[is.na(data[,1]),1] = 1
+
+data[data[,1]==0,1] = 1e-16
+data[,2] = log2(data[,2])
+
+dds_res = data.frame(log2FoldChange=data[,2],padj=data[,1])
+
+library(graphics)
+library(gplots)
+library(ggplot2)
+pdf("oleg_rnaseq_volcanoPlot_30perc_log2FC37_withNUMBERS.pdf")
+plot(dds_res$log2FoldChange,-log10(dds_res$padj),xlab=expression('Log'[2]*' Fold Change ( shNT / shH2AFV )'),
+              ylab=expression('-Log'[10]*' Q-values'),col=alpha("grey",.5))
+abline(v=-.37,lty = 2,col="grey")
+abline(v=.37,lty = 2,col="grey")
+abline(h=-log10(0.05),lty = 2,col="grey")
+points(dds_res$log2FoldChange[abs(dds_res$log2FoldChange)>.37 & dds_res$padj<0.05],
+       -log10(dds_res$padj)[abs(dds_res$log2FoldChange)>.37 & dds_res$padj<0.05],
+      col="red")
+legend("topright", paste("shNT:",length(which(dds_res$log2FoldChange>.37 & dds_res$padj<0.05))), bty="n") 
+legend("topleft", paste("shH2AFV:",length(which(dds_res$log2FoldChange<(-.37) & dds_res$padj<0.05))), bty="n") 
+dev.off()
+
+pdf("oleg_rnaseq_volcanoPlot_30perc_log2FC37_withoutNUMBERS.pdf")
+plot(dds_res$log2FoldChange,-log10(dds_res$padj),xlab=expression('Log'[2]*' Fold Change ( shNT / shH2AFV )'),
+              ylab=expression('-Log'[10]*' Q-values'),col=alpha("grey",.5))
+abline(v=-.37,lty = 2,col="grey")
+abline(v=.37,lty = 2,col="grey")
+abline(h=-log10(0.05),lty = 2,col="grey")
+points(dds_res$log2FoldChange[abs(dds_res$log2FoldChange)>.37 & dds_res$padj<0.05],
+       -log10(dds_res$padj)[abs(dds_res$log2FoldChange)>.37 & dds_res$padj<0.05],
+      col="red")
+dev.off()
