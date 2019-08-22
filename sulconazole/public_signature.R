7a93e6b8df4c94a5bdb933849847b30ef01a92aa
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-08-22 13:07:07 +0800

    Update public_signature.R

--- sulconazole/public_signature.R
+++ sulconazole/public_signature.R
@@ -96,3 +96,99 @@ dev.off()
 pdf("survival_GBMLG_Signature_K4Cut_pval_table.pdf")
 kmTCGA(clinical, explanatory.names="signature",  pval = TRUE,conf.int = FALSE, risk.table=TRUE,palette = c("red","blue","green","orange"))
 dev.off()
+
+########################################################################################
+
+remb <- read.table("2019-08-21_Rembrandt_expression.txt", 
+                     header=TRUE, row.names=1)
+remb <- t(remb)
+remb_sig <- remb[rownames(remb) %in% signature$Gene.symbol,]
+
+track= signature[match(rownames(remb_sig),signature[,1]),2]
+track[track=="EGFR"]=1
+track[track=="Mesenchymal"]=2
+track[track=="GenerationOfNeurons"]=3
+track=as.numeric(track)
+colores=c("#ffb3ba","#baffc9","#bae1ff")
+rlab=as.character(colores[track])
+
+remb.pheno <- read.table("2019-08-21_Rembrandt_pheno.txt", 
+                     header=TRUE, row.names=1)
+
+ctrack = as.character(remb.pheno$Histology)
+ctrack[ctrack=="Astrocytoma"]=1
+ctrack[ctrack=="GBM"]=2
+ctrack[ctrack=="Mixed glioma"]=3
+ctrack[ctrack=="Oligodendroglioma"]=4
+ctrack[ctrack=="Non-tumor"]=5
+ctrack[is.na(ctrack)]=6
+ctrack[ctrack=="Unknown"]=6
+
+colores=c("red","black","#ffb347","#966fd6","blue","grey")
+clab=as.character(colores[as.numeric(ctrack)])
+
+
+remb_sig_centered = remb_sig - rowMeans(remb_sig)
+
+remb_sig_centered[remb_sig_centered > 4] = 4
+remb_sig_centered[remb_sig_centered < (-4)] = -4
+
+colors <- rev(colorRampPalette( (brewer.pal(11, "RdBu")) )(11))
+
+
+png("heatmap_Rembrandt_Signature.png",width= 7.25,
+  height= 7.25,units="in",
+  res=1200,pointsize=4)
+x=heatmap.2(as.matrix(remb_sig_centered),col=colors,scale="none", trace="none",
+              distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+labRow = "",labCol = "",xlab="Rembrandt Patient Sample", ylab="Signature Genes",key.title="",
+         RowSideColors=rlab,ColSideColors=clab)
+dev.off()
+
+pdf("heatmap_Rembrandt_labels.pdf")
+plot.new()
+legend("center",legend=c("EGFR","Mesenchymal","GenerationOfNeurons",
+                           "Astrocytoma","GBM","Mixed glioma","Oligodendroglioma","Non-tumor","Unknown"),
+       fill=c("#ffb3ba","#baffc9","#bae1ff","red","black","#ffb347","#966fd6","blue","grey"), border=T, bty="n" )
+
+dev.off()
+########################################################################################
+########################################################################################
+
+hc <- as.hclust( x$colDendrogram )
+groups=cutree( hc, k=5)
+
+track=as.numeric(groups)
+colores=c("red","blue","green","orange","purple")
+clab=(colores[track])
+
+png("heatmap_Rembrandt_Signature_K5Cut.png",width= 7.25,
+  height= 7.25,units="in",
+  res=1200,pointsize=4)
+
+heatmap.2(as.matrix(remb_sig_centered),col=colors,scale="none", trace="none",
+              distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+labRow = "",labCol = "",xlab="Rembrandt Patient Sample", ylab="Signature Genes",key.title="",
+         RowSideColors=rlab,ColSideColors=clab)
+dev.off()
+
+library(RTCGA.clinical)
+
+clinical <- data.frame(times = remb.pheno$survival,
+                       bcr_patient_barcode = rownames(remb.pheno),
+                       patient.vital_status = as.numeric(remb.pheno$status),
+                       signature = as.factor(groups))
+# alive=0 and dead=1
+clinical$bcr_patient_barcode == colnames(GBMLGG_sig_centered)
+
+pdf("survival_Rembrandt_Signature_K5Cut_pval.pdf")
+kmTCGA(clinical, explanatory.names="signature",  pval = TRUE,conf.int = FALSE, risk.table=FALSE,palette = c("red","blue","green","orange","purple"))
+dev.off()
+
+pdf("survival_Rembrandt_Signature_K5Cut.pdf")
+kmTCGA(clinical, explanatory.names="signature",  pval = FALSE,conf.int = FALSE, risk.table=FALSE,palette = c("red","blue","green","orange","purple"))
+dev.off()
+
+pdf("survival_GBMLG_Signature_K4Cut_pval_table.pdf")
+kmTCGA(clinical, explanatory.names="signature",  pval = TRUE,conf.int = FALSE, risk.table=TRUE,palette = c("red","blue","green","orange"))
+dev.off()
