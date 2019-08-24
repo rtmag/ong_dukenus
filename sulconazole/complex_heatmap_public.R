596e5d8f8cabbec3a31fbfdd0ee385ae43ced488
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-08-25 00:41:18 +0800

    Update complex_heatmap_public.R

--- sulconazole/complex_heatmap_public.R
+++ sulconazole/complex_heatmap_public.R
@@ -1,1 +1,61 @@
+library(ComplexHeatmap)
+options(scipen=999)
+library(graphics)
+library(gplots)
+library(factoextra)
+library(RColorBrewer)
+
+
+# READ AND PARSE SIGNATURES
+signature <- read.csv("3_pathway_signature.csv",stringsAsFactors=FALSE)
+signature[signature$Geneset_for_CMAP_analysis=="KOBAYASHI_EGFR_SIGNALING_24HR_DN",2] <- "EGFR"
+signature[signature$Geneset_for_CMAP_analysis=="VERHAAK_GLIOBLASTOMA_MESENCHYMAL",2] <- "Mesenchymal"
+signature[signature$Geneset_for_CMAP_analysis=="generation of neurons",2] <- "GenerationOfNeurons"
+
+#READ EXPRESSION TABLE
+GBMLGG <- read.table("2019-08-21_TCGA_GBMLGG_expression.txt", header=TRUE, row.names=1)
+GBMLGG <- t(GBMLGG)
+
+#READ PHENO TABLE
+GBMLGG.pheno <- read.table("2019-08-21_TCGA_GBMLGG_pheno.txt", header=TRUE, row.names=1)
+
+#CHECK GRADE OF ASTRO AND GBM
+ctrack = data.frame(hist=as.character(GBMLGG.pheno$Histology),grade=as.character(GBMLGG.pheno$Grade))
+table(ctrack[ctrack$hist=="Astrocytoma",2])
+table(ctrack[ctrack$hist=="GBM",2])
+
+#REMOVE other than ASTRO AND GBM AND keep non NA from expression and pheno tables
+ix = as.character(GBMLGG.pheno$Histology)!="Oligoastrocytoma" & 
+     as.character(GBMLGG.pheno$Histology)!="Oligodendroglioma" & 
+     !is.na(GBMLGG.pheno$Grade) & !is.na(GBMLGG.pheno$Histology)
+GBMLGG_astro_gbm <- GBMLGG[,ix]
+GBMLGG_astro_gbm.pheno <- GBMLGG.pheno[ix,]
+
+#GET SIGNATURE GENES ONLY
+GBMLGG_astro_gbm_sig <- GBMLGG_astro_gbm[rownames(GBMLGG_astro_gbm) %in% signature$Gene.symbol,]
+
+track= signature[match(rownames(GBMLGG_astro_gbm_sig),signature[,1]),2]
+
+#create label track with grade
+ctrack = as.character(GBMLGG_astro_gbm.pheno$Grade)
+
+
+GBMLGG_sig_centered = GBMLGG_astro_gbm_sig - rowMeans(GBMLGG_astro_gbm_sig)
+
+
+x=heatmap.2(as.matrix(GBMLGG_sig_centered),col=colors,scale="none", trace="none",
+              distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+labRow = "",labCol = "",xlab="TCGA GBM-LGG Patient Sample", ylab="Signature Genes",key.title="",
+         RowSideColors=rlab,ColSideColors=clab)
+
+rdend = dendsort(hclust(dist(mat)))
+
+column_ha = HeatmapAnnotation(Grade = ctrack, col = list(Grade = c("II" = "black", "III" = "grey", "IV" = "red")))
+row_ha = rowAnnotation(Signature = track,
+              col = list(Signature = c("EGFR" = "#ffb3ba", "Mesenchymal" = "#baffc9", "GenerationOfNeurons" = "#bae1ff")))
+
+Heatmap(GBMLGG_sig_centered,
+show_row_names = FALSE,show_column_names = FALSE,name = "Expression",
+column_title="TCGA GBM-LGG Patients", column_title_side = "bottom", row_title="Gene Signature", row_title_side = "right",
+bottom_annotation = column_ha, right_annotation = row_ha)
 
