341a907a7cd777fd02a4330a8b2633e99e6157c0
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-09-04 16:35:35 +0800

    Update small_dataset_heatmap.R

--- sulconazole/small_dataset_heatmap.R
+++ sulconazole/small_dataset_heatmap.R
@@ -47,9 +47,131 @@ grid.ftable <- function(d, padding = unit(4, "mm"), ...) {
   invisible(g)
 }
 
-
 # READ AND PARSE SIGNATURES
 signature <- read.csv("3_pathway_signature.csv",stringsAsFactors=FALSE)
 signature[signature$Geneset_for_CMAP_analysis=="KOBAYASHI_EGFR_SIGNALING_24HR_DN",2] <- "EGFR"
 signature[signature$Geneset_for_CMAP_analysis=="VERHAAK_GLIOBLASTOMA_MESENCHYMAL",2] <- "Mesenchymal"
 signature[signature$Geneset_for_CMAP_analysis=="generation of neurons",2] <- "GenerationOfNeurons"
+###
+
+#READ EXPRESSION TABLE
+dataset <- read.table("2019-09-04_Freije_expression.txt", header=TRUE, row.names=1)
+dataset <- t(dataset)
+
+#READ PHENO TABLE
+dataset.pheno <- read.table("2019-09-04_Freije_pheno.txt", header=TRUE, row.names=1)
+
+#CHECK GRADE OF ASTRO AND GBM
+ctrack = data.frame(hist=as.character(dataset.pheno$Histology),grade=as.character(dataset.pheno$Grade))
+table(ctrack[ctrack$hist=="Astrocytoma",2])
+table(ctrack[ctrack$hist=="GBM",2])
+
+#REMOVE other than ASTRO AND GBM AND keep non NA from expression and pheno tables
+ix = as.character(dataset.pheno$Histology)!="Mixed glioma" & 
+     as.character(dataset.pheno$Histology)!="Oligodendroglioma" & 
+     !is.na(dataset.pheno$Grade) 
+dataset_astro_gbm <- dataset[,ix]
+dataset_astro_gbm.pheno <- dataset.pheno[ix,]
+
+#GET SIGNATURE GENES ONLY
+dataset_astro_gbm_sig <- dataset_astro_gbm[rownames(dataset_astro_gbm) %in% signature$Gene.symbol,]
+
+track= signature[match(rownames(dataset_astro_gbm_sig),signature[,1]),2]
+
+#create label track with grade
+ctrack = as.character(dataset_astro_gbm.pheno$Grade)
+ctrack2 = as.character(dataset_astro_gbm.pheno$Subtype.original)
+
+dataset_sig_centered = dataset_astro_gbm_sig - rowMeans(dataset_astro_gbm_sig)
+dataset_sig_centered[dataset_sig_centered >= 6] = 6
+dataset_sig_centered[dataset_sig_centered <= (-6)] = -6
+
+column_ha = HeatmapAnnotation(Grade = ctrack,
+                              col = list(Grade = c("II" = "black", "III" = "grey", "IV" = "red")
+                                      ))
+row_ha = rowAnnotation(Signature = track,show_annotation_name = FALSE,
+              col = list(Signature = c("GenerationOfNeurons" = "#bae1ff","EGFR" = "#ffb3ba", "Mesenchymal" = "#baffc9")))
+
+png("complexHeatmap_Freije_Forced3Groups_Kmeans3.png",width= 7.25,
+  height= 7.25,units="in",
+  res=1200,pointsize=4)
+Heatmap(dataset_sig_centered,
+show_row_names = FALSE,show_column_names = FALSE,name = "Expression",row_dend_reorder = T, column_dend_reorder = F,
+column_title="Freije Patients", column_title_side = "bottom", row_title="Gene Signature", row_title_side = "right",
+bottom_annotation = column_ha, right_annotation = row_ha,
+        clustering_distance_columns = "pearson",column_km = 3, column_km_repeats = 100,
+        clustering_distance_rows = "pearson",row_split =track)
+dev.off()
+
+png("complexHeatmap_Freije_noKmeans.png",width= 7.25,
+  height= 7.25,units="in",
+  res=1200,pointsize=4)
+Heatmap(dataset_sig_centered,
+show_row_names = FALSE,show_column_names = FALSE,name = "Expression",row_dend_reorder = T, column_dend_reorder = F,
+column_title="Freije Patients", column_title_side = "bottom", row_title="Gene Signature", row_title_side = "right",
+bottom_annotation = column_ha, right_annotation = row_ha,
+        clustering_distance_columns = "pearson",
+        clustering_distance_rows = "pearson",row_split =track)
+dev.off()
+################################################################################################
+################################################################################################
+################################################################################################
+# Phillips
+
+#READ EXPRESSION TABLE
+dataset <- read.table("2019-09-04_Phillips_expression.txt", header=TRUE, row.names=1)
+dataset <- t(dataset)
+
+#READ PHENO TABLE
+dataset.pheno <- read.table("2019-09-04_Phillips_pheno.txt", header=TRUE, row.names=1)
+
+#CHECK GRADE OF ASTRO AND GBM
+ctrack = data.frame(hist=as.character(dataset.pheno$Histology),grade=as.character(dataset.pheno$Grade))
+table(ctrack[ctrack$hist=="Astrocytoma",2])
+table(ctrack[ctrack$hist=="GBM",2])
+
+#REMOVE other than ASTRO AND GBM AND keep non NA from expression and pheno tables
+ix = !is.na(dataset.pheno$Grade) 
+dataset_astro_gbm <- dataset[,ix]
+dataset_astro_gbm.pheno <- dataset.pheno[ix,]
+
+#GET SIGNATURE GENES ONLY
+dataset_astro_gbm_sig <- dataset_astro_gbm[rownames(dataset_astro_gbm) %in% signature$Gene.symbol,]
+
+track= signature[match(rownames(dataset_astro_gbm_sig),signature[,1]),2]
+
+#create label track with grade
+ctrack = as.character(dataset_astro_gbm.pheno$Grade)
+ctrack2 = as.character(dataset_astro_gbm.pheno$Subtype.original)
+
+dataset_sig_centered = dataset_astro_gbm_sig - rowMeans(dataset_astro_gbm_sig)
+dataset_sig_centered[dataset_sig_centered >= 6] = 6
+dataset_sig_centered[dataset_sig_centered <= (-6)] = -6
+
+column_ha = HeatmapAnnotation(Grade = ctrack,
+                              col = list(Grade = c("II" = "black", "III" = "grey", "IV" = "red")
+                                      ))
+row_ha = rowAnnotation(Signature = track,show_annotation_name = FALSE,
+              col = list(Signature = c("GenerationOfNeurons" = "#bae1ff","EGFR" = "#ffb3ba", "Mesenchymal" = "#baffc9")))
+
+png("complexHeatmap_Phillips_Forced3Groups_Kmeans3.png",width= 7.25,
+  height= 7.25,units="in",
+  res=1200,pointsize=4)
+Heatmap(dataset_sig_centered,
+show_row_names = FALSE,show_column_names = FALSE,name = "Expression",row_dend_reorder = T, column_dend_reorder = F,
+column_title="Phillips Patients", column_title_side = "bottom", row_title="Gene Signature", row_title_side = "right",
+bottom_annotation = column_ha, right_annotation = row_ha,
+        clustering_distance_columns = "pearson",column_km = 3, column_km_repeats = 100,
+        clustering_distance_rows = "pearson",row_split =track)
+dev.off()
+
+png("complexHeatmap_Phillips_noKmeans.png",width= 7.25,
+  height= 7.25,units="in",
+  res=1200,pointsize=4)
+Heatmap(dataset_sig_centered,
+show_row_names = FALSE,show_column_names = FALSE,name = "Expression",row_dend_reorder = T, column_dend_reorder = F,
+column_title="Phillips Patients", column_title_side = "bottom", row_title="Gene Signature", row_title_side = "right",
+bottom_annotation = column_ha, right_annotation = row_ha,
+        clustering_distance_columns = "pearson",
+        clustering_distance_rows = "pearson",row_split =track)
+dev.off()
