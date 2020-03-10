a16a7ccbb2929a5f45a97b5ca650540668b9b80e
Roberto Tirado Magallanes <rtmag4@gmail.com>
2020-03-11 00:07:55 +0800

    Create h2a_chipseq.sh

---
+++ h2a_chipseq.sh
@@ -0,0 +1,5 @@
+~/myPrograms/sra-tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump --split-files -Z SRR9102954 > H2A.Z1_rep1_U2OS_SRR9102954.fastq &
+~/myPrograms/sra-tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump --split-files -Z SRR9102955 > H2A.Z2_rep1_U2OS_SRR9102955.fastq &
+~/myPrograms/sra-tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump --split-files -Z SRR9102956 > H2A.Z1_rep2_U2OS_SRR9102956.fastq &
+~/myPrograms/sra-tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump --split-files -Z SRR9102957 > H2A.Z2_rep2_U2OS_SRR9102957.fastq &
+############
