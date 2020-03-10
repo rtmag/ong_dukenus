e4e561c20389f08818af24dcb182fb1a20f96f2d
Roberto Tirado Magallanes <rtmag4@gmail.com>
2020-03-10 23:38:02 +0800

    Update ATAC-Seq_RNA-Seq_integration.R

--- atac_rna_int_redo/ATAC-Seq_RNA-Seq_integration.R
+++ atac_rna_int_redo/ATAC-Seq_RNA-Seq_integration.R
@@ -1,7 +1,7 @@
-rna = readRDS("MASTER_RNASEQ_TABLE_OLEG.rds")
+rna = readRDS("/Users/wone/CSI/ong/atacseq_redo/MASTER_RNASEQ_TABLE_OLEG.rds")
 
-atac = read.table(pipe('more ATAC_TSS_500bp_20bp.rmat|grep -v "#"|grep -v "shNT_1_rmdup"'),sep="\t",header=F)
-bed = read.table("hg19_tss_knownCanonical_noUnasembled.bed",sep="\t",stringsAsFactors=F)
+atac = read.table(pipe('more /Users/wone/CSI/ong/atacseq_redo/ATAC_TSS_500bp_20bp.rmat|grep -v "#"|grep -v "shNT_1_rmdup"'),sep="\t",header=F)
+bed = read.table("/Users/wone/CSI/ong/atacseq_redo/hg19_tss_knownCanonical_noUnasembled.bed",sep="\t",stringsAsFactors=F)
 rownames(atac) = make.unique(as.character(bed[,4]), sep = "_")
 
 nt1 = rowSums(atac[,1:50])
@@ -35,3 +35,30 @@ abline(h=0,lty=3)
 abline(v=0,lty=3)
 dev.off()
  
+targets=read.table("~/CSI/sjlab/deseq2/HallMark_e2f_targets.txt")
+targets<-as.character(unique(targets[,1]))
+ 
+pdf("ATAC-RNA_integration_TSS_E2F_targets.pdf")
+smoothScatter(-log2(rna$PostFC),atacfc,xlim=c(-3,3), ylim=c(-2,2),nrpoints=0,
+              xlab=expression('RNA-Seq Log'[2]*' Fold Change ( shH2AFV / shNT )'),
+              ylab=expression('ATAC-Seq Log'[2]*' Fold Change ( shH2AFV / shNT )'),
+             )
+abline(h=0,lty=3)
+abline(v=0,lty=3)
+points(-log2(rna$PostFC)[rna[,1] %in% targets],
+       atacfc[names(atacfc) %in% targets],
+      col="red",pch=20)
+dev.off()
+ 
+scp -P 60035 remap2018_REST_nr_macs2_hg38_v1_2.bed root@172.18.149.78:./root/ong_dukenus/TF_targets
+annotatePeaks.pl remap2018_REST_nr_macs2_hg38_v1_2.bed hg38 -annStats REST_remap.annStats > REST_remap.anno 
+
+more remap2018_REST_all_macs2_hg38_v1_2.bed |grep -i "Neural" > rest_neural_remap.bed
+scp -P 60035 rest_neural_remap.bed root@172.18.149.78:/root/ong_dukenus/TF_targets
+annotatePeaks.pl rest_neural_remap.bed hg38 -annStats rest_neural_remap.annStats > rest_neural_remap.anno 
+
+library(TFregulomeR)
+
+rest <- loadPeaks(id = "GTRD-EXP010296_HSA_U87_REST", includeMotifOnly = TRUE)
+write.table(rest,"rest_gbm_quy.bed",sep="\t",quote=F,row.names=F,col.names=F)
+
