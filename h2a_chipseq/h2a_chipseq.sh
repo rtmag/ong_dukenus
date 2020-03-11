a0ed12627f79020ac4a1b8dbffe27fc94a24b3bf
Roberto Tirado Magallanes <rtmag4@gmail.com>
2020-03-12 00:39:02 +0800

    Update h2a_chipseq.sh

--- h2a_chipseq/h2a_chipseq.sh
+++ h2a_chipseq/h2a_chipseq.sh
@@ -1,13 +1,13 @@
-~/myPrograms/sra-tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump --split-files --gzip -Z SRR9102954 > H2A.Z1_rep1_U2OS_SRR9102954.fastq 
-~/myPrograms/sra-tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump --split-files --gzip -Z SRR9102955 > H2A.Z2_rep1_U2OS_SRR9102955.fastq 
-~/myPrograms/sra-tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump --split-files --gzip -Z SRR9102956 > H2A.Z1_rep2_U2OS_SRR9102956.fastq 
-~/myPrograms/sra-tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump --split-files --gzip -Z SRR9102957 > H2A.Z2_rep2_U2OS_SRR9102957.fastq 
+~/myPrograms/sra-tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump --split-files --gzip -Z SRR9102954 > H2A.Z1_rep1_U2OS_SRR9102954.fastq.gz 
+~/myPrograms/sra-tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump --split-files --gzip -Z SRR9102955 > H2A.Z2_rep1_U2OS_SRR9102955.fastq.gz
+~/myPrograms/sra-tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump --split-files --gzip -Z SRR9102956 > H2A.Z1_rep2_U2OS_SRR9102956.fastq.gz
+~/myPrograms/sra-tools/sratoolkit.2.9.0-centos_linux64/bin/fastq-dump --split-files --gzip -Z SRR9102957 > H2A.Z2_rep2_U2OS_SRR9102957.fastq.gz 
 ############
 
-trim_galore --illumina -q 20 --fastqc -o ./ H2A.Z1_rep1_U2OS_SRR9102954.fastq &
-trim_galore --illumina -q 20 --fastqc -o ./ H2A.Z2_rep1_U2OS_SRR9102955.fastq &
-trim_galore --illumina -q 20 --fastqc -o ./ H2A.Z1_rep2_U2OS_SRR9102956.fastq &
-trim_galore --illumina -q 20 --fastqc -o ./ H2A.Z2_rep2_U2OS_SRR9102957.fastq &
+trim_galore --illumina -q 20 --fastqc -o ./ H2A.Z1_rep1_U2OS_SRR9102954.fastq.gz &
+trim_galore --illumina -q 20 --fastqc -o ./ H2A.Z2_rep1_U2OS_SRR9102955.fastq.gz &
+trim_galore --illumina -q 20 --fastqc -o ./ H2A.Z1_rep2_U2OS_SRR9102956.fastq.gz &
+trim_galore --illumina -q 20 --fastqc -o ./ H2A.Z2_rep2_U2OS_SRR9102957.fastq.gz &
 
 ############
 
