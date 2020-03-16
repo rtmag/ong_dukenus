a3281a35904fd7338b3df8a607f1d7aec6e1ff32
Roberto Tirado Magallanes <rtmag4@gmail.com>
2020-03-16 23:07:45 +0800

    Create chrom_chipseq.bash

---
+++ h2a_chipseq/chrom_chipseq.bash
@@ -0,0 +1,46 @@
+############
+trim_galore --illumina --paired -q 20 --fastqc -o /root/ong_dukenus/chrom_chipseq/fastq/ \
+/root/ong_dukenus/chrom_chipseq/TS543-H3K27ac/TS543_H3K27ac_CHKSEI85218090013_1_FT.fq.gz \
+/root/ong_dukenus/chrom_chipseq/TS543-H3K27ac/TS543-H3K27ac_CHKSEI85218090013_2_FT.fq.gz &
+
+trim_galore --illumina --paired -q 20 --fastqc -o /root/ong_dukenus/chrom_chipseq/fastq/ \
+/root/ong_dukenus/chrom_chipseq/TS543-H3K4me3/H3K4me3_CHKSEI85218090014_1_FT.fq.gz \
+/root/ong_dukenus/chrom_chipseq/TS543-H3K4me3/H3K4me3_CHKSEI85218090014_2_FT.fq.gz &
+
+trim_galore --illumina --paired -q 20 --fastqc -o /root/ong_dukenus/chrom_chipseq/fastq/ \
+/root/ong_dukenus/chrom_chipseq/TS543_input/T543_input_CHKSEI85218090010_1_FT.fq.gz \
+/root/ong_dukenus/chrom_chipseq/TS543_input/T543_input_CHKSEI85218090010_2_FT.fq.gz &
+
+############
+/root/myPrograms/STAR/bin/STAR --genomeDir /root/resources/hg19_noanno/ \
+--readFilesCommand zcat \
+--runThreadN 35 \
+--alignIntronMax 1 \
+--outFilterScoreMinOverLread 0.40 \
+--outFilterMatchNminOverLread 0.40 \
+--alignEndsType EndToEnd \
+--readFilesIn H2A.Z1_rep1_U2OS_SRR9102954_trimmed.fq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix H2A.Z1_rep1_
+
+/root/myPrograms/STAR/bin/STAR --genomeDir /root/resources/hg38_noanno/ \
+--readFilesCommand zcat \
+--runThreadN 35 \
+--alignIntronMax 1 \
+--outFilterScoreMinOverLread 0.40 \
+--outFilterMatchNminOverLread 0.40 \
+--alignEndsType EndToEnd \
+--readFilesIn H2A.Z2_rep1_U2OS_SRR9102955_trimmed.fq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix H2A.Z2_rep1_
+
+/root/myPrograms/STAR/bin/STAR --genomeDir /root/resources/hg38_noanno/ \
+--readFilesCommand zcat \
+--runThreadN 35 \
+--alignIntronMax 1 \
+--outFilterScoreMinOverLread 0.40 \
+--outFilterMatchNminOverLread 0.40 \
+--alignEndsType EndToEnd \
+--readFilesIn H2A.Z1_rep2_U2OS_SRR9102956_trimmed.fq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix H2A.Z1_rep2_
