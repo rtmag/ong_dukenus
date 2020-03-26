584cd2618bd89fde69ba77177f689a3904ac2448
Roberto Tirado Magallanes <rtmag4@gmail.com>
2020-03-26 16:43:04 +0800

    Update chrom_chipseq.bash

--- h2a_chipseq/chrom_chipseq.bash
+++ h2a_chipseq/chrom_chipseq.bash
@@ -135,14 +135,16 @@ H3K27ac_peaks.broadPeak \
 H3K4me3_peaks.broadPeak \
 shNT_IP_broad_.001_peaks.broadPeak \
 -names ATAC_DOWN H3K27ac H3K4me3 H2AZ -header > multi_gbm.bed
+#R
 library(VennDiagram)
-multi_bed<-read.table("multi_H3K27me3.bed",header=T,stringsAsFactors=F)
-#formating
-list.multi<-list(NHM=which(multi_bed$NHM==1),NB=which(multi_bed$NB==1),NC=which(multi_bed$NC==1),NBC=which(multi_bed$NBC==1))
+multi_bed<-read.table("multi_gbm.bed",header=T,stringsAsFactors=F)
+list.multi<-list(ATAC_DOWN=which(multi_bed$ATAC_DOWN==1),
+                 H2AZ=which(multi_bed$H2AZ==1),
+                 H3K4me3=which(multi_bed$H3K4me3==1),
+                 H3K27ac=which(multi_bed$H3K27ac==1))
 vp <- venn.diagram(list.multi,fill=c("red", "green","blue","orange"), alpha = .3, 
 cex = 1,lty =2,cat.cex=1, filename = NULL,margin=.1,col=NA)
 #plotting
-pdf("multi_H3K27me3.pdf")
+pdf("multi_gbm.pdf")
 grid.draw(vp)
 dev.off()
-© 2020 GitHub, Inc.
