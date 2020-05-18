542a8492fd7b851f229116facec8881e04890434
Roberto Tirado Magallanes <rtmag4@gmail.com>
2020-05-18 15:09:09 +0800

    Create GEO_prep.bash

---
+++ GEO_prep.bash
@@ -0,0 +1,13 @@
+mv 1_3502DukeNus_TS543-NT-031117_hs_i9_r1.fastq.gz shNT_rep1_R1.fastq.gz
+mv 1_3502DukeNus_TS543-NT-031117_hs_i9_r2.fastq.gz shNT_rep1_R2.fastq.gz
+mv 2_3502DukeNus_TS543-143-031117_hs_i10_r1.fastq.gz shH2_I_rep1_R1.fastq.gz
+mv 2_3502DukeNus_TS543-143-031117_hs_i10_r2.fastq.gz shH2_I_rep1_R2.fastq.gz
+mv 3_3502DukeNus_TS543-400-031117_hs_i11_r1.fastq.gz shH2_II_rep1_R1.fastq.gz
+mv 3_3502DukeNus_TS543-400-031117_hs_i11_r2.fastq.gz shH2_II_rep1_R2.fastq.gz
+mv 4_3502DukeNus_TS543-NT-241117_hs_i12_r1.fastq.gz shNT_rep2_R1.fastq.gz
+mv 4_3502DukeNus_TS543-NT-241117_hs_i12_r2.fastq.gz shNT_rep2_R2.fastq.gz
+mv 5_3502DukeNus_TS543-143-241117_hs_i13_r1.fastq.gz shH2_I_rep2_R1.fastq.gz
+mv 5_3502DukeNus_TS543-143-241117_hs_i13_r2.fastq.gz shH2_I_rep2_R2.fastq.gz
+mv 6_3502DukeNus_TS543-400-241117_hs_i14_r1.fastq.gz shH2_II_rep2_R1.fastq.gz
+mv 6_3502DukeNus_TS543-400-241117_hs_i14_r2.fastq.gz shH2_II_rep2_R2.fastq.gz
+################################
