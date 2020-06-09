d0e820b72d895053f532d3a778994ba0a2d797df
Roberto Tirado Magallanes <rtmag4@gmail.com>
2020-06-10 04:46:38 +0800

    Update REDO_rna_seq.bash

--- REDO_rna_seq.bash
+++ REDO_rna_seq.bash
@@ -32,3 +32,55 @@ cat /home/oleg/DATA/H2AFV_PROJECT/gsc_543_RNA-seq_raw/sh400_1-109823740/sh400-1_
 cat /home/oleg/DATA/H2AFV_PROJECT/gsc_543_RNA-seq_raw/sh400_2-109815738/sh400-2_S4_L001_R2_001.fastq.gz /home/oleg/DATA/H2AFV_PROJECT/gsc_543_RNA-seq_raw/sh400_2-109815738/sh400-2_S4_L002_R2_001.fastq.gz /home/oleg/DATA/H2AFV_PROJECT/gsc_543_RNA-seq_raw/sh400_2-109815738/sh400-2_S4_L003_R2_001.fastq.gz /home/oleg/DATA/H2AFV_PROJECT/gsc_543_RNA-seq_raw/sh400_2-109815738/sh400-2_S4_L004_R2_001.fastq.gz > /home/oleg/DATA/H2AFV_PROJECT/gsc_543_RNA-seq_raw/sh400_2-109815738/sh400-2_S4_.fastq.gz
 
 cat /home/oleg/DATA/H2AFV_PROJECT/gsc_543_RNA-seq_raw/sh400_3-109823741/sh400-3_S1_L001_R2_001.fastq.gz /home/oleg/DATA/H2AFV_PROJECT/gsc_543_RNA-seq_raw/sh400_3-109823741/sh400-3_S1_L002_R2_001.fastq.gz /home/oleg/DATA/H2AFV_PROJECT/gsc_543_RNA-seq_raw/sh400_3-109823741/sh400-3_S1_L003_R2_001.fastq.gz /home/oleg/DATA/H2AFV_PROJECT/gsc_543_RNA-seq_raw/sh400_3-109823741/sh400-3_S1_L004_R2_001.fastq.gz > /home/oleg/DATA/H2AFV_PROJECT/gsc_543_RNA-seq_raw/sh400_3-109823741/sh400-3_S1_.fastq.gz
+
+
+#######
+ls *L001_R1*| perl -pe 's/(.+)L001_R1_001.fastq.gz/cat $1L001_R1_001.fastq.gz $1L002_R1_001.fastq.gz $1L003_R1_001.fastq.gz $1L004_R1_001.fastq.gz \> $1R1.fastq.gz/g'
+
+cat sh143-1_S6_L001_R1_001.fastq.gz sh143-1_S6_L002_R1_001.fastq.gz sh143-1_S6_L003_R1_001.fastq.gz sh143-1_S6_L004_R1_001.fastq.gz > sh143-1_S6_R1.fastq.gz
+cat sh143-2_S3_L001_R1_001.fastq.gz sh143-2_S3_L002_R1_001.fastq.gz sh143-2_S3_L003_R1_001.fastq.gz sh143-2_S3_L004_R1_001.fastq.gz > sh143-2_S3_R1.fastq.gz
+cat sh143-3_S9_L001_R1_001.fastq.gz sh143-3_S9_L002_R1_001.fastq.gz sh143-3_S9_L003_R1_001.fastq.gz sh143-3_S9_L004_R1_001.fastq.gz > sh143-3_S9_R1.fastq.gz
+cat sh202-1_S8_L001_R1_001.fastq.gz sh202-1_S8_L002_R1_001.fastq.gz sh202-1_S8_L003_R1_001.fastq.gz sh202-1_S8_L004_R1_001.fastq.gz > sh202-1_S8_R1.fastq.gz
+cat sh202-2_S7_L001_R1_001.fastq.gz sh202-2_S7_L002_R1_001.fastq.gz sh202-2_S7_L003_R1_001.fastq.gz sh202-2_S7_L004_R1_001.fastq.gz > sh202-2_S7_R1.fastq.gz
+cat sh202-3_S5_L001_R1_001.fastq.gz sh202-3_S5_L002_R1_001.fastq.gz sh202-3_S5_L003_R1_001.fastq.gz sh202-3_S5_L004_R1_001.fastq.gz > sh202-3_S5_R1.fastq.gz
+cat sh400-1_S2_L001_R1_001.fastq.gz sh400-1_S2_L002_R1_001.fastq.gz sh400-1_S2_L003_R1_001.fastq.gz sh400-1_S2_L004_R1_001.fastq.gz > sh400-1_S2_R1.fastq.gz
+cat sh400-2_S4_L001_R1_001.fastq.gz sh400-2_S4_L002_R1_001.fastq.gz sh400-2_S4_L003_R1_001.fastq.gz sh400-2_S4_L004_R1_001.fastq.gz > sh400-2_S4_R1.fastq.gz
+cat sh400-3_S1_L001_R1_001.fastq.gz sh400-3_S1_L002_R1_001.fastq.gz sh400-3_S1_L003_R1_001.fastq.gz sh400-3_S1_L004_R1_001.fastq.gz > sh400-3_S1_R1.fastq.gz
+
+
+ls *L001_R1*| perl -pe 's/(.+)L001_R1_001.fastq.gz/cat $1L001_R2_001.fastq.gz $1L002_R2_001.fastq.gz $1L003_R2_001.fastq.gz $1L004_R2_001.fastq.gz \> $1R2.fastq.gz/g'
+
+
+
+cat sh143-1_S6_L001_R2_001.fastq.gz sh143-1_S6_L002_R2_001.fastq.gz sh143-1_S6_L003_R2_001.fastq.gz sh143-1_S6_L004_R2_001.fastq.gz > sh143-1_S6_R2.fastq.gz
+cat sh143-2_S3_L001_R2_001.fastq.gz sh143-2_S3_L002_R2_001.fastq.gz sh143-2_S3_L003_R2_001.fastq.gz sh143-2_S3_L004_R2_001.fastq.gz > sh143-2_S3_R2.fastq.gz
+cat sh143-3_S9_L001_R2_001.fastq.gz sh143-3_S9_L002_R2_001.fastq.gz sh143-3_S9_L003_R2_001.fastq.gz sh143-3_S9_L004_R2_001.fastq.gz > sh143-3_S9_R2.fastq.gz
+cat sh202-1_S8_L001_R2_001.fastq.gz sh202-1_S8_L002_R2_001.fastq.gz sh202-1_S8_L003_R2_001.fastq.gz sh202-1_S8_L004_R2_001.fastq.gz > sh202-1_S8_R2.fastq.gz
+cat sh202-2_S7_L001_R2_001.fastq.gz sh202-2_S7_L002_R2_001.fastq.gz sh202-2_S7_L003_R2_001.fastq.gz sh202-2_S7_L004_R2_001.fastq.gz > sh202-2_S7_R2.fastq.gz
+cat sh202-3_S5_L001_R2_001.fastq.gz sh202-3_S5_L002_R2_001.fastq.gz sh202-3_S5_L003_R2_001.fastq.gz sh202-3_S5_L004_R2_001.fastq.gz > sh202-3_S5_R2.fastq.gz
+cat sh400-1_S2_L001_R2_001.fastq.gz sh400-1_S2_L002_R2_001.fastq.gz sh400-1_S2_L003_R2_001.fastq.gz sh400-1_S2_L004_R2_001.fastq.gz > sh400-1_S2_R2.fastq.gz
+cat sh400-2_S4_L001_R2_001.fastq.gz sh400-2_S4_L002_R2_001.fastq.gz sh400-2_S4_L003_R2_001.fastq.gz sh400-2_S4_L004_R2_001.fastq.gz > sh400-2_S4_R2.fastq.gz
+cat sh400-3_S1_L001_R2_001.fastq.gz sh400-3_S1_L002_R2_001.fastq.gz sh400-3_S1_L003_R2_001.fastq.gz sh400-3_S1_L004_R2_001.fastq.gz > sh400-3_S1_R2.fastq.gz
+
+
+
+###################################################
+mv sh143-1_S6_R1.fastq.gz RNA_shH2_I_rep1_R1.fastq.gz
+mv sh143-1_S6_R2.fastq.gz RNA_shH2_I_rep1_R2.fastq.gz
+mv sh143-2_S3_R1.fastq.gz RNA_shH2_I_rep2_R1.fastq.gz
+mv sh143-2_S3_R2.fastq.gz RNA_shH2_I_rep2_R2.fastq.gz
+mv sh143-3_S9_R1.fastq.gz RNA_shH2_I_rep3_R1.fastq.gz
+mv sh143-3_S9_R2.fastq.gz RNA_shH2_I_rep3_R2.fastq.gz
+mv sh202-1_S8_R1.fastq.gz RNA_shNT_rep1_R1.fastq.gz
+mv sh202-1_S8_R2.fastq.gz RNA_shNT_rep1_R2.fastq.gz
+mv sh202-2_S7_R1.fastq.gz RNA_shNT_rep2_R1.fastq.gz
+mv sh202-2_S7_R2.fastq.gz RNA_shNT_rep2_R2.fastq.gz
+mv sh202-3_S5_R1.fastq.gz RNA_shNT_rep3_R1.fastq.gz
+mv sh202-3_S5_R2.fastq.gz RNA_shNT_rep3_R2.fastq.gz
+mv sh400-1_S2_R1.fastq.gz RNA_shH2_II_rep1_R1.fastq.gz
+mv sh400-1_S2_R2.fastq.gz RNA_shH2_II_rep1_R2.fastq.gz
+mv sh400-2_S4_R1.fastq.gz RNA_shH2_II_rep2_R1.fastq.gz
+mv sh400-2_S4_R2.fastq.gz RNA_shH2_II_rep2_R2.fastq.gz
+mv sh400-3_S1_R1.fastq.gz RNA_shH2_II_rep3_R1.fastq.gz
+mv sh400-3_S1_R2.fastq.gz RNA_shH2_II_rep3_R2.fastq.gz
+################################################
