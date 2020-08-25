913675b5eadcd61372c9c8baffe42829b7cb3999
Roberto Tirado Magallanes <rtmag4@gmail.com>
2020-08-25 16:06:08 +0800

    Update chrom_chipseq_REDOafterDeletion.bash

--- h2a_chipseq/chrom_chipseq_REDOafterDeletion.bash
+++ h2a_chipseq/chrom_chipseq_REDOafterDeletion.bash
@@ -6,3 +6,27 @@ trim_galore --illumina --paired -q 20 --fastqc -o /root/ong_dukenus/chrom_chipse
 /root/ong_dukenus/chrom_chipseq/fastq/TS543_H3K27ac_CHKSEI85218090013_1_FT.fq.gz \
 /root/ong_dukenus/chrom_chipseq/fastq/TS543-H3K27ac_CHKSEI85218090013_2_FT.fq.gz &
 #######################################
+/root/myPrograms/STAR/bin/STAR --genomeDir /root/resources/hg19_noanno/ \
+--readFilesCommand zcat \
+--runThreadN 35 \
+--alignIntronMax 1 \
+--outFilterScoreMinOverLread 0.40 \
+--outFilterMatchNminOverLread 0.40 \
+--alignEndsType EndToEnd \
+--readFilesIn /root/ong_dukenus/chrom_chipseq/fastq/TS543_H3K27ac_CHKSEI85218090013_1_FT.fq.gz \
+/root/ong_dukenus/chrom_chipseq/fastq/TS543-H3K27ac_CHKSEI85218090013_2_FT.fq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/ong_dukenus/chrom_chipseq/bam/H3K4me3_
+
+/root/myPrograms/STAR/bin/STAR --genomeDir /root/resources/hg19_noanno/ \
+--readFilesCommand zcat \
+--runThreadN 35 \
+--alignIntronMax 1 \
+--outFilterScoreMinOverLread 0.40 \
+--outFilterMatchNminOverLread 0.40 \
+--alignEndsType EndToEnd \
+--readFilesIn /root/ong_dukenus/chrom_chipseq/fastq/T543_input_CHKSEI85218090010_1_FT.fq.gz \
+/root/ong_dukenus/chrom_chipseq/fastq/T543_input_CHKSEI85218090010_2_FT.fq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/ong_dukenus/chrom_chipseq/bam/input_
+#######################################
