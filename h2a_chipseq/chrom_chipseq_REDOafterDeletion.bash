774b06d42a93f6c1e72ce59699085b9aa07a30a1
Roberto Tirado Magallanes <rtmag4@gmail.com>
2020-08-25 17:28:28 +0800

    Update chrom_chipseq_REDOafterDeletion.bash

--- h2a_chipseq/chrom_chipseq_REDOafterDeletion.bash
+++ h2a_chipseq/chrom_chipseq_REDOafterDeletion.bash
@@ -16,7 +16,7 @@ trim_galore --illumina --paired -q 20 --fastqc -o /root/ong_dukenus/chrom_chipse
 --readFilesIn /root/ong_dukenus/chrom_chipseq/fastq/TS543_H3K27ac_CHKSEI85218090013_1_FT.fq.gz \
 /root/ong_dukenus/chrom_chipseq/fastq/TS543-H3K27ac_CHKSEI85218090013_2_FT.fq.gz \
 --outSAMtype BAM SortedByCoordinate \
---outFileNamePrefix /root/ong_dukenus/chrom_chipseq/bam/H3K4me3_
+--outFileNamePrefix /root/ong_dukenus/chrom_chipseq/bam/H3K27ac_
 
 /root/myPrograms/STAR/bin/STAR --genomeDir /root/resources/hg19_noanno/ \
 --readFilesCommand zcat \
@@ -29,4 +29,17 @@ trim_galore --illumina --paired -q 20 --fastqc -o /root/ong_dukenus/chrom_chipse
 /root/ong_dukenus/chrom_chipseq/fastq/T543_input_CHKSEI85218090010_2_FT.fq.gz \
 --outSAMtype BAM SortedByCoordinate \
 --outFileNamePrefix /root/ong_dukenus/chrom_chipseq/bam/input_
+#######################################
+java -jar /root/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
+I=/root/ong_dukenus/chrom_chipseq/bam/input_Aligned.sortedByCoord.out.bam \
+O=/root/ong_dukenus/chrom_chipseq/bam/input_rmdup.bam \
+M=/root/ong_dukenus/chrom_chipseq/bam/input.mfile
+
+java -jar /root/myPrograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
+I=/root/ong_dukenus/chrom_chipseq/bam/H3K27ac_Aligned.sortedByCoord.out.bam \
+O=/root/ong_dukenus/chrom_chipseq/bam/H3K27ac_rmdup.bam \
+M=/root/ong_dukenus/chrom_chipseq/bam/H3K27ac.mfile
+
+samtools index input_rmdup.bam &
+samtools index H3K27ac_rmdup.bam
 #######################################
