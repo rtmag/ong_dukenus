7fe57744ecb218cdc98345b651815f1489d83e58
Roberto Tirado Magallanes <rtmag4@gmail.com>
2020-08-27 05:17:11 +0800

    Update rose_plot.R

--- h2a_chipseq/rose_plot.R
+++ h2a_chipseq/rose_plot.R
@@ -58,3 +58,53 @@ text(nc[,1],nc[,2],label=se_labels,cex=1)
 segments(nc[,1]+(nc[,3]/2),nc[,2],(length(rankBy_vector):1)[1:20], rankBy_vector[signalOrder][1:20])
 
 dev.off()
+
+
+
+####
+# BROAD
+options(bitmapType="cairo")
+wceName = "present"
+
+stitched_regions <- read.delim(file= "H3K27ac_gbm_broad_6Columns_12KB_STITCHED_TSS_DISTAL_ENHANCER_REGION_MAP.txt",sep="\t")
+annot<- read.table(pipe("cut -f1,2,3,4,16 H3K27ac_gbm_SuperEnhancers_broad.anno"),sep="\t",header = T,stringsAsFactors=F)
+
+stitched_regions_x<- cbind( stitched_regions, annot[match(stitched_regions[,1], annot[,1]),5])
+
+#perform WCE subtraction. Using pipeline table to match samples to proper background.
+rankBy_factor = colnames(stitched_regions)[7]
+prefix = unlist(strsplit(rankBy_factor,'_'))[1]
+
+wceName = colnames(stitched_regions)[8]
+rankBy_vector = as.numeric(stitched_regions[,7])-as.numeric(stitched_regions[,8])
+
+#SETTING NEGATIVE VALUES IN THE rankBy_vector to 0
+rankBy_vector[rankBy_vector < 0] <- 0
+
+#FIGURING OUT THE CUTOFF
+cutoff_options <- list()
+cutoff_options$absolute <- 9202.9535
+
+#These are the super-enhancers
+superEnhancerRows <- which(rankBy_vector> cutoff_options$absolute)
+typicalEnhancers = setdiff(1:nrow(stitched_regions),superEnhancerRows)
+
+#MAKING HOCKEY STICK PLOT
+pdf("superEnhancer_H3K27ac_names_Plot_points_broad.pdf")
+signalOrder = order(rankBy_vector,decreasing=TRUE)
+
+plot(length(rankBy_vector):1,rankBy_vector[signalOrder], col='red',xlab="Super enhancers ranked by H3K27ac signal",ylab="H3K27ac ChIP-Seq signal - Input ",pch=19,cex=2)
+abline(h=cutoff_options$absolute,col='grey',lty=2)
+abline(v=length(rankBy_vector)-length(superEnhancerRows),col='grey',lty=2)
+lines(length(rankBy_vector):1,rankBy_vector[signalOrder],lwd=4, col='red')
+
+se_labels<-as.character(stitched_regions_x[signalOrder,9][1:20])
+library(wordcloud)
+nc=wordlayout( (length(rankBy_vector):1)[1:20], rankBy_vector[signalOrder][1:20],words=se_labels)
+nc[,1] <- nc[,1]-500
+nc[5:dim(nc)[1],2] <- nc[5:dim(nc)[1],2]+50000
+text(nc[,1],nc[,2],label=se_labels,cex=1)
+
+segments(nc[,1]+(nc[,3]/2),nc[,2],(length(rankBy_vector):1)[1:20], rankBy_vector[signalOrder][1:20])
+
+dev.off()
