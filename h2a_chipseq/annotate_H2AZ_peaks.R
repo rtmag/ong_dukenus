6b00beb4797ea18045420106a6a9dd871b451e00
Roberto Tirado Magallanes <rtmag4@gmail.com>
2021-02-10 01:25:55 +0800

    Update annotate_H2AZ_peaks.R

--- h2a_chipseq/annotate_H2AZ_peaks.R
+++ h2a_chipseq/annotate_H2AZ_peaks.R
@@ -2,7 +2,7 @@
 
 system("annotatePeaks.pl /root/ong_dukenus/h2az/shNT_IP_broad_-log10_5.bed hg19 -annStats shNT_IP_broad_-log10_5.annStats > shNT_IP_broad_-log10_5.anno")
 
-pdf("H2AZ_-log10_5_pie.pdf")
+pdf("H2AZ_-log10_5_pie.pdf",width=9)
 res=read.table(pipe("more shNT_IP_broad_-log10_5.annStats |cut -f1,2,4"), sep="\t",header=F)
 i1 = which(res[,1]=="Annotation")[2]+1
 i2 = dim(res)[1]
@@ -10,19 +10,23 @@ res = res[ i1:i2,]
 tdown = as.numeric(as.character(res[,2]))
 names(tdown) = res[,1]
 names(tdown) = paste(names(tdown)," ",round(tdown/sum(tdown)*100,digits=2),"%",sep="")
-tdown = tdown[tdown>50]
-pie(sort(tdown), main=,cex=.8)
+ix = tdown>500
+combined = sum(tdown[!ix])
+names(combined) = paste("Others ",round(combined/sum(tdown)*100,digits=2),"%",sep="")
+merged = c(tdown[ix],combined)
+pie(sort(merged),cex=1.2, main=paste0("Genomic distribution of ",sum(tdown)," H2AZ peaks"))
 dev.off()
 
-pdf("ATAC-down_log2.pdf")
+pdf("H2AZ_-log10_5_enrichmentBars.pdf")
 par(mar=c(11.1,4.1,4.1,2))
-res=read.table(pipe("more ATAC-down.annStats |cut -f1,2,4"), sep="\t",header=F)
+res=read.table(pipe("more shNT_IP_broad_-log10_5.annStats |cut -f1,2,4"), sep="\t",header=F)
 i1 = which(res[,1]=="Annotation")[2]+1
 i2 = dim(res)[1]
 res = res[ i1:i2,]
 tdown = as.numeric(as.character(res[,3]))
 names(tdown) = res[,1]
-tdown = tdown[as.numeric(as.character(res[,2]))>50]
-barplot(sort(tdown),las=2,ylim=c(-4,6),ylab="Log2 Enrichment over random genomic background",col="lightblue3")
+tdown = tdown[as.numeric(as.character(res[,2]))>500]
+barplot(sort(tdown),las=2,ylim=c(-4,6),ylab="Log2 Enrichment over a random genomic background",col="lightblue3",yaxt='n')
+axis(2, at=c(-4, -2, 0, 2,4,6), labels=c("-4", "-2", "0","2","4","6"), cex.axis=1, srt=45, col.ticks = "black")
 abline(h=0)
 dev.off()
